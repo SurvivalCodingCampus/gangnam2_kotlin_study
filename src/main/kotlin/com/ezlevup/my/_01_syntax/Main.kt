40e3de6f78ccd7943c7e3f4e9ad577178c2330c7
이영현 <hong.git@yahoo.com>
2025-10-28 16:13:54 +0900

    args 파라미터 전달

--- src/main/kotlin/com/ezlevup/my/_01_syntax/Main.kt
+++ src/main/kotlin/com/ezlevup/my/_01_syntax/Main.kt
@@ -1,5 +1,11 @@
 package com.ezlevup.my._01_syntax
 
-fun main() {
+/*
+프로그램 인자 전달 방법
+args 파라미터에 값을 전달하려면 Run > Edit Configurations로 이동합니다.
+Program arguments 필드에 전달할 값을 공백으로 구분하여 입력합니다. 예: arg1 arg2 arg3.
+ */
+fun main(args: Array<String>) {
     println("Hello world!")
+    println(args.contentToString())
 }
