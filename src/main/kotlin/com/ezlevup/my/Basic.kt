d6a9e87f3d4315befbb3a235b617016cd38c7485
이영현 <hong.git@yahoo.com>
2025-10-28 20:00:30 +0900

    Create Basic.kt
    for, while 연습

---
+++ src/main/kotlin/com/ezlevup/my/Basic.kt
@@ -0,0 +1,21 @@
+package com.ezlevup.my
+
+fun main() {
+    val items = listOf("apple", "banana", "kiwifruit")
+    for (item in items) {
+        println(item)
+    }
+
+    for (index in items.indices) {
+        println(index)
+        println("item at $index is ${items[index]}")
+    }
+
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+
+    
+}
