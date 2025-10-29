e0bb91d43b766f784351af10218850938be8a79d
unknown <hr.jeon28@gmail.com>
2025-10-29 19:50:36 +0900

    fix(Cleric): Correct pray() recovery logic

--- src/main/kotlin/com/luca/kotlinstudy/_02_instance_class/Cleric.kt
+++ src/main/kotlin/com/luca/kotlinstudy/_02_instance_class/Cleric.kt
@@ -21,24 +21,29 @@ class Cleric(var name: String = "", var hp: Int = MAX_HP, var mp: Int = MAX_MP) 
 
     fun pray(prayTime: Int): Int {
         val oldMp = mp // 기존 mp
-        var recoveryMp = mp + prayTime + Random.nextInt(0,3) // 회복
-        var actualRecovery = 0 // 회복량 체크
+        var recoveryMp = mp + prayTime + Random.nextInt(0, 3) // 회복
 
-        if(mp == MAX_MP) { // 최대치라면
+        if (mp == MAX_MP) { // 최대치라면
             println("이미 마나가 최대치입니다.")
             return 0
         }
 
-        if ( recoveryMp > MAX_MP ) { // 회복이 50보다 넘어설 때
+        val recoveryAmount = prayTime + Random.nextInt(0, 3)
+        recoveryMp = mp + recoveryAmount
+
+        if (recoveryMp > MAX_MP) { // 회복이 최대치를 넘을 때
             recoveryMp = MAX_MP
-            actualRecovery = recoveryMp - oldMp // 회복량 체크
-            mp = MAX_MP
+        }
+        val actualRecovery = recoveryMp - oldMp //(회복량 체크)
+        mp = recoveryMp
+
+        if (mp == MAX_MP) {
             println("마나가 최대치로 회복 되었습니다. 현재 MP: $mp ")
         } else {
-            actualRecovery = recoveryMp - oldMp //회복량 체크
-            mp = recoveryMp
             println("마나가 $actualRecovery 만큼 회복 되었습니다. 현재 MP: $mp ")
         }
+
         return actualRecovery
     }
+
 }
