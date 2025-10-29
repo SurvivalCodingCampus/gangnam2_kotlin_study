f040b29320277dc049b3ea03e2f14aa2820e7b13
whoamixzerone <whoamixzerone@gmail.com>
2025-10-29 14:36:10 +0900

    feat: 연습문제 2 추가

--- src/main/kotlin/com/survivaalcoding/kotlinstudy/02_instance_class/example/Cleric.kt
+++ src/main/kotlin/com/survivaalcoding/kotlinstudy/02_instance_class/example/Cleric.kt
@@ -3,4 +3,24 @@ package com.survivaalcoding.kotlinstudy.`02_instance_class`.example
 class Cleric(val name: String, var hp: Int = 50, var mp: Int = 10) {
     val maxHp: Int = 50
     val maxMp: Int = 10
+
+    fun selfAid() {
+        if (mp < 5) {
+            println("mp가 부족합니다. 현재 mp: $mp")
+            return
+        }
+
+        if (hp >= maxHp) {
+            println("hp가 최대입니다.")
+            return
+        }
+
+        val hpRecovery = 10
+        val mpCost = 5
+
+        mp -= mpCost
+        hp += hpRecovery
+        println("회복 스킬을 사용해 mp가 $mpCost 감소하고, hp가 $hpRecovery 증가합니다.")
+        println("현재 hp: $hp, mp: $mp")
+    }
 }
---
+++ src/main/kotlin/com/survivaalcoding/kotlinstudy/02_instance_class/example/Main.kt
@@ -0,0 +1,9 @@
+package com.survivaalcoding.kotlinstudy.`02_instance_class`.example
+
+fun main() {
+    val clericA = Cleric("클레릭A", 30)
+
+    clericA.selfAid()
+    clericA.selfAid()
+    clericA.selfAid()
+}
