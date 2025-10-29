bf77f4bc168df48ddc954bc190a4629e6f3c7160
MisterJerry <misterjerry12345@gmail.com>
2025-10-29 16:16:57 +0900

    feat: Cleric클래스의 일부 멤버변수를 생성자로 변경

--- src/main/kotlin/_251029_kotlin_oop/Cleric.kt
+++ src/main/kotlin/_251029_kotlin_oop/Cleric.kt
@@ -1,20 +1,36 @@
 package _251029_kotlin_oop
 
-class Cleric() {
-    val name: String = ""
-    var hp: Int = 50
+class Cleric(
+    val name: String = "",
+    var hp: Int = 40,
     var mp: Int = 10
+) {
     val maxHp: Int = 50
     val maxMp: Int = 10
 
-    fun selfAid() { // MP 5소비, 체력 끝까지 회복
-        hp = maxHp
+    fun selfAid() {
+        if(mp>5){ // MP 5소비, 체력 끝까지 회복
+            hp = maxHp
+            mp -= 5
+        }
+        else{
+            return
+        }
     }
 
-    fun pray(time: Int):Int { // mp를 초당 1회복 하고 보너스로 0~2 추가회복, 최대MP를 넘을 수 없음
+    fun pray(time: Int): Int { // mp를 초당 1회복 하고 보너스로 0~2 추가회복, 최대MP를 넘을 수 없음
         //실제로 회복된 MP량을 반환
         val randomRange = 0..2
         val randomBonus = randomRange.random()
-        return randomBonus
+        val expected = time + randomBonus //mp회복 예상치
+        if (mp + expected > maxMp) {
+            val result = maxMp-mp
+            mp=maxMp
+            return result
+        } else {
+            mp+=expected
+            return expected
+        }
+
     }
 }
