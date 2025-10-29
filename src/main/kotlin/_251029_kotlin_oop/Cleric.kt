da599603a1475bd333e28c351cd96ff8a4b1cda2
MisterJerry <misterjerry12345@gmail.com>
2025-10-29 16:04:14 +0900

    feat: 연습문제3 selfAid 메서드 추가 완료

--- src/main/kotlin/_251029_kotlin_oop/Cleric.kt
+++ src/main/kotlin/_251029_kotlin_oop/Cleric.kt
@@ -10,4 +10,11 @@ class Cleric() {
     fun selfAid() { // MP 5소비, 체력 끝까지 회복
         hp = maxHp
     }
+
+    fun pray(time: Int):Int { // mp를 초당 1회복 하고 보너스로 0~2 추가회복, 최대MP를 넘을 수 없음
+        //실제로 회복된 MP량을 반환
+        val randomRange = 0..2
+        val randomBonus = randomRange.random()
+        return randomBonus
+    }
 }
