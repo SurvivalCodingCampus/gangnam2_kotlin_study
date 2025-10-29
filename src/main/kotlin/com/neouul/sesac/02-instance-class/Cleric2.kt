d7d1652517166d1f5cbc558ba75122542d9fbda4
Neouul <fluctu0q0@gmail.com>
2025-10-29 15:45:52 +0900

    Feat: 다른 버전으로 구현한 Cleric2 클래스 추가

--- src/main/kotlin/com/neouul/sesac/02-instance-class/Cleric2.kt
+++ src/main/kotlin/com/neouul/sesac/02-instance-class/Cleric2.kt
@@ -1,4 +1,26 @@
 package com.neouul.sesac.`02-instance-class`
 
-class Cleric2 {
+import kotlin.random.Random
+
+// 2. 바디 내부에 주어진 속성을 초기치로 선언한 버전
+class Cleric2(var name: String = "") {
+    var HP: Int = 50
+    val maxHP: Int = 50
+    var MP: Int = 10
+    val maxMP: Int = 10
+
+    // 메서드는 동일
+    fun selfAid() {
+        MP -= 5
+        HP = maxHP
+    }
+
+    fun pray(sec: Int): Int {
+        val recentMP = MP
+        val randomNumber = Random.nextInt(3)    // 0~2 난수 생성
+        MP += sec + randomNumber
+        MP = if (MP > maxMP) maxMP else MP
+
+        return MP - recentMP
+    }
 }
