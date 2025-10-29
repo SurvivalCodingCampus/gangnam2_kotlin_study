9762bac923b085aff003826d291c46e423628f25
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-29 14:34:16 +0900

    feat: 연습문제 2 selfAid() 메소드 작성

--- src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
+++ src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
@@ -1,5 +1,13 @@
 package com.survival.kotlinstudy.`02_instance_class`
 
-class Cleric(var name: String,var hp: Int,maxHp:Int=50,var mp: Int,maxMp: Int=50) {
-
+class Cleric(var name: String, var hp: Int, var mp: Int) {
+    val maxHp: Int = 50
+    val maxMp: Int = 50
+    fun selfAid() {
+        if (mp < 5) return
+        else {
+            mp -= 5
+            hp = maxHp
+        }
+    }
 }
