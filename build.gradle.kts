8594f41cdff86faa236a72b721fd314f2c27fa54
Neouul <fluctu0q0@gmail.com>
2025-10-29 14:10:53 +0900

    Chore: Gradle DSL을 groovy에서 Kotlin으로 변경

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+group = "com.survivalcoding"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
