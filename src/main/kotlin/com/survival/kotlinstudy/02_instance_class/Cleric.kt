767d4f72f11067581d4ab5f689e557ec6cc74d6e
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-29 14:42:05 +0900

    feat: 연습문제 3 pray() 메소드 작성

--- src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
+++ src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
@@ -1,13 +1,23 @@
 package com.survival.kotlinstudy.`02_instance_class`
 
+import kotlin.random.Random
+
 class Cleric(var name: String, var hp: Int, var mp: Int) {
     val maxHp: Int = 50
     val maxMp: Int = 50
+
     fun selfAid() {
         if (mp < 5) return
         else {
             mp -= 5
             hp = maxHp
         }
+    }
+
+
+    fun pray(time: Int): Int {
+        val randomNumber = Random.nextInt(0, 3)
+        val recoveredMp = time + randomNumber
+        return if (maxMp < mp + recoveredMp) maxMp - mp else recoveredMp
     }
 }
