6689e02066c6b6c83f079c9b146205ab4b9349be
Jieun Lee <geniee1750@gmail.com>
2025-10-29 14:44:40 +0900

    [과제] feat : 연습문제 1 풀이 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Cleric.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Cleric.kt
@@ -1,14 +1,17 @@
 package com.survivalcoding.kotlinstudy.`02_instance_class`
 
+import java.sql.Time
+
 
 class Cleric (
+    // 연습문제 1 . 클래스 작성
     var name : String,
-    var hp : Int,
-    var mp : Int,
+    var hp : Int = 50,
+    var mp : Int = 10,
     val maxHp: Int = 50,
     val maxMp: Int = 10
 ) {
-    // 메소드 추가
+    // 연습문제 2. selfAid 메소드
     fun selfAid() {
         if (mp >= 5 ) {
             mp -= 5 // mp 5 소비
