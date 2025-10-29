76bb93045fdee230e771978e3c77a7b0eba37733
jonghyuk <harry7408@naver.com>
2025-10-29 14:17:04 +0900

    feat: Problem2

--- src/main/kotlin/com/harry/instance_class/Cleric.kt
+++ src/main/kotlin/com/harry/instance_class/Cleric.kt
@@ -4,17 +4,38 @@ const val MAX_HP = 50
 const val MAX_MP = 10
 
 fun main() {
-
+    val cleric = Cleric(
+        name = "성직자1",
+        hp = 50,
+        mp = 5,
+    )
+    println("before SelfAid()")
+    printClericStatus(cleric)
+    cleric.selfAid()
+    println("After SelfAid()")
+    printClericStatus(cleric)
 }
 
 class Cleric(
-    val name: String,
+    var name: String,
     var hp: Int,
     val maxHp: Int = MAX_HP,
-    val mp: Int,
+    var mp: Int,
     val maxMp: Int = MAX_MP,
 ) {
-    fun attach() {
-        hp-=10
+    fun selfAid() {
+        if (this.mp < 5) {
+            println("MP가 부족합니다")
+        } else if (this.hp == MAX_HP) {
+            println("체력이 이미 최대치 입니다")
+        } else {
+            this.mp -= 5
+            this.hp = MAX_HP
+            println("체력 회복 완료")
+        }
     }
+}
+
+fun printClericStatus(cleric: Cleric) {
+    println("name : ${cleric.name}\nhp : ${cleric.hp}\nmp: ${cleric.mp}")
 }
