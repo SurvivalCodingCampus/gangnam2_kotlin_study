334fd39b91a4ee6fd881d50750c4547bc3009d3c
Jieun Lee <geniee1750@gmail.com>
2025-10-29 14:22:57 +0900

    [과제] feat : 연습문제 1 풀이 제출

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Cleric.kt
@@ -0,0 +1,12 @@
+package com.survivalcoding.kotlinstudy.`02_instance_class`
+
+
+class Cleric (
+    var name : String,
+    var hp : Int,
+    var mp : Int,
+    val maxhp : Int = 50,
+    val maxmp : Int = 10
+) {
+
+}
