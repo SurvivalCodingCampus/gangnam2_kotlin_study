398bf7d6602c37e40905696302cbe872f30700fb
wantfree8937 <qkrtmdduq112@naver.com>
2025-10-29 14:55:06 +0900

    Practice1

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Cleric.kt
@@ -0,0 +1,12 @@
+package com.survivalcoding.kotlinstudy.`02_instance_class`
+
+
+class Cleric(
+    var name: String,
+    var hp: Int = 50,
+    val maxHp: Int = 50,
+    var mp: Int = 10,
+    val maxMp: Int = 10
+) {
+
+}
