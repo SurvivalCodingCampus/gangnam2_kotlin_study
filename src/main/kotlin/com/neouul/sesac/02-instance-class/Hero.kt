18d78f164d369f002ba1368aa16eed56675616cf
Neouul <fluctu0q0@gmail.com>
2025-10-29 14:08:48 +0900

    Feat: 02-instance-class 예제 추가

--- src/main/kotlin/com/neouul/sesac/02-instance-class/Hero.kt
+++ src/main/kotlin/com/neouul/sesac/02-instance-class/Hero.kt
@@ -1,4 +1,22 @@
 package com.neouul.sesac.`02-instance-class`
 
-class Hero {
+class Hero(
+    var name: String = "",
+    var hp: Int = 0
+){
+    // 멤버 변수
+    // name
+    // hp
+
+    fun attack() {
+        // 로컬 변수
+    }
+
+    fun run() {
+
+    }
+
+    fun sleep() {
+
+    }
 }
