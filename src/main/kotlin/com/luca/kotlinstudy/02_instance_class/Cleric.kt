af438b1ca2173166bcea9e7ff28d7691ba4184c8
unknown <hr.jeon28@gmail.com>
2025-10-29 15:01:41 +0900

    feat(Cleric): Implement selfAid() for selfHealing

--- src/main/kotlin/com/luca/kotlinstudy/02_instance_class/Cleric.kt
+++ src/main/kotlin/com/luca/kotlinstudy/02_instance_class/Cleric.kt
@@ -3,6 +3,17 @@ package com.luca.kotlinstudy.`02_instance_class`
 const val MAX_HP = 50
 const val MAX_MP = 10
 
-class Cleric(var name: String = "", var hp: Int = MAX_HP, var mp: Int = MAX_MP) {
 
+class Cleric(var name: String = "", var hp: Int = MAX_HP, var mp: Int = MAX_MP) {
+    fun selfAid() {
+        if (mp >= 5 && hp < MAX_HP ) {
+            mp -= 5
+            hp = MAX_HP
+            println("HP: $hp, MP: $mp ")
+        }else if(hp == MAX_HP) {
+            println("이미 체력이 최대치입니다.")
+        } else {
+            println("마나가 부족합니다.")
+        }
+    }
 }
