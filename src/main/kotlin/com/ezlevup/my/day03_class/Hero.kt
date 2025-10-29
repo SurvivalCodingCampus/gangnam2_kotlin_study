ad517009f3a97fda16b20b3642964ba7d4383c20
이영현 <hong.git@yahoo.com>
2025-10-29 15:50:16 +0900

    fix: 공격에서 hp가 마이너스가 되는 부분 수정

--- src/main/kotlin/com/ezlevup/my/day03_class/Hero.kt
+++ src/main/kotlin/com/ezlevup/my/day03_class/Hero.kt
@@ -1,13 +1,15 @@
 package com.ezlevup.my.day03_class
 
-class Hero {
+class Hero(var name: String, var hp: Int) {
+    val attackPower: Int = 10
     fun attack() {
         println("attack")
+        hp = if ((hp - attackPower) < 0) 0 else hp - attackPower
     }
 }
 
 fun main() {
-    val hero = Hero()
+    val hero = Hero(name = "lee", hp = 100)
     hero.attack()
 }
 
---
+++ src/test/kotlin/com/ezlevup/my/day03_class/HeroTest.kt
@@ -0,0 +1,30 @@
+package com.ezlevup.my.day03_class
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HeroTest {
+    @Test
+    fun `Hero의 attack() 메서드를 실행하면 hp가 10만큼 줄어든다`() {
+        // given(준비)
+        val hero = Hero(name = "lee", hp = 100)
+
+        // when(실행)
+        hero.attack()
+
+        // then(검증)
+        assertEquals(90, hero.hp)
+    }
+
+    @Test
+    fun `hp가 0일때 attack()을 해도 0이어야함`() {
+        // given
+        val hero = Hero(name = "lee", hp = 0)
+
+        // when
+        hero.attack()
+
+        // then
+        assertEquals(0, hero.hp)
+    }
+}
