20bdf12dbe0e5dc4910aa7a57ac5ede74ed5f187
jonghyuk <harry7408@naver.com>
2025-10-29 14:25:42 +0900

    feat: Problem 3

--- src/main/kotlin/com/harry/instance_class/Cleric.kt
+++ src/main/kotlin/com/harry/instance_class/Cleric.kt
@@ -1,19 +1,28 @@
 package com.harry.instance_class
 
+import kotlin.random.Random
+
 const val MAX_HP = 50
 const val MAX_MP = 10
 
 fun main() {
+    val prayTime = 1
     val cleric = Cleric(
         name = "성직자1",
         hp = 50,
         mp = 5,
     )
-    println("before SelfAid()")
+    println("before selfAid")
     printClericStatus(cleric)
     cleric.selfAid()
-    println("After SelfAid()")
+    println("After selfAid")
     printClericStatus(cleric)
+
+    println("before pray")
+    println("${cleric.mp}")
+    println("${prayTime}동안 회복한 MP 양 : ${cleric.pray(prayTime)}")
+    println("After pray")
+    println("${cleric.mp}")
 }
 
 class Cleric(
@@ -32,6 +41,17 @@ class Cleric(
             this.mp -= 5
             this.hp = MAX_HP
             println("체력 회복 완료")
+        }
+    }
+
+    fun pray(prayTime: Int) : Int {
+        val healResult = prayTime + Random.nextInt(2)
+        if (this.mp+healResult > MAX_MP) {
+            println("MP 회복이 불가능 합니다.")
+            return 0
+        } else {
+            this.mp+=healResult
+            return healResult
         }
     }
 }
