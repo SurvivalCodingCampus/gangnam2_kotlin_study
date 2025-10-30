71dbc26ecc79ade372591a567731d540bd732c07
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-10-30 17:01:22 +0900

    feat: 클래스

--- src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Hero.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/02_instance_class/Hero.kt
@@ -1,14 +1,49 @@
 package com.survivalcoding.kotlinstudy.`02_instance_class`
 
-fun main() {
-    val hero = Hero()
-    val hero2 = Hero(name = "홍길동", hp = 10)
-    hero2.name = ""
+import kotlin.math.max
+import kotlin.random.Random
 
-    println("잘 되나!!!")
+fun main() {
+    val hero1 = Hero(name = "홍길동", hp = 100)
+    val hero2 = hero1
+    hero2.hp = 200
+
+    println(hero1.hp)
+
+    val hero3 = Hero(name = "한석봉", hp = 50)
+
+    Hero.MONEY = 90
+
+    println(Hero.MONEY)
+
+    Hero.setRandomMoney()
+
 }
 
-class Hero(var name: String = "", var hp: Int = 0) {
+const val MAX_HP: Int = 10 // 컴파일 타임
+
+// 동적
+class Hero(
+    var name: String = "", // (값 == 불변)
+    var hp: Int = MAX_HP,
+) {
+    //    // java static 정적
+    companion object {
+        var MONEY = 100
+
+        const val RANDOM_MONEY = 100
+
+        fun setRandomMoney() {
+            MONEY = Random.nextInt(1000)
+            this.MONEY = 100
+
+            val hero = Hero("홍길동")
+            hero.name = ""
+
+            max(10, 100)
+        }
+    }
+
     // 멤버변수, field, 전역 변수
     // name
     // hp
@@ -26,7 +61,7 @@ class Hero(var name: String = "", var hp: Int = 0) {
     }
 
     fun sleep() {
-
+        MONEY = 200
     }
 }
 
