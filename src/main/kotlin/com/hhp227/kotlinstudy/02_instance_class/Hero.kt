df1f8d05f8199a76af0eb1e941aa2e9961eee032
hhp227 <hong227@naver.com>
2025-10-30 16:09:54 +0900

    feat: Cleric 연습문제 기능 추가

--- src/main/kotlin/com/hhp227/kotlinstudy/02_instance_class/Hero.kt
+++ src/main/kotlin/com/hhp227/kotlinstudy/02_instance_class/Hero.kt
@@ -1,6 +1,25 @@
 package com.hhp227.kotlinstudy.`02_instance_class`
 
-class Hero(var name: String = "", var hp: Int) {
+import kotlin.random.Random
+
+const val MAX_HP = 10
+
+class Hero(
+    var name: String = "",
+    var hp: Int = MAX_HP
+) {
+    companion object {
+        var MONEY = 100
+
+        //const val random = Random.nextInt(100) // 불가 Random.nextInt()는 런타임에 동작
+
+        fun setRandomMoney() {
+            MONEY = Random.nextInt(1000)
+
+            //println("${name}의 소지금을 추가했다.") // 접근 불가
+        }
+    }
+
     fun attack() {
         // 로컬 변수
         hp -= 10
