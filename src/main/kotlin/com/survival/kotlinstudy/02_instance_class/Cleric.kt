afe1ff0368cd6bcf87321f14d57a61a743211744
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-30 15:46:19 +0900

    feat: 연습문제 1 최대 HP,최대 MP 필드 공유 자원으로 변경

--- src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
+++ src/main/kotlin/com/survival/kotlinstudy/02_instance_class/Cleric.kt
@@ -3,29 +3,32 @@ package com.survival.kotlinstudy.`02_instance_class`
 import kotlin.random.Random
 
 class Cleric(var name: String, var hp: Int = 50, var mp: Int = 10) {
-    val maxHp: Int = 50
-    val maxMp: Int = 10
 
     fun selfAid() {
         if (mp < 5) return
         mp -= 5
-        hp = maxHp
+        hp = MAX_HP
     }
 
 
     fun pray(time: Int): Int {
-        if (time < 1 || mp == maxMp) return 0
+        if (time < 1 || mp == MAX_MP) return 0
         val randomNumber = Random.nextInt(0, 3)
         val recoveredMp = time + randomNumber
         val beforeMp = mp
 
-        if (mp + recoveredMp > maxMp) {
-            mp = maxMp
+        if (mp + recoveredMp > MAX_MP) {
+            mp = MAX_MP
         } else {
             mp += recoveredMp
         }
 
         val actuallyMp = mp - beforeMp
         return actuallyMp
+    }
+
+    companion object {
+        const val MAX_HP = 50
+        const val MAX_MP = 10
     }
 }
