4aeee5b311c91b848f41f27530c5c4a2c61c7454
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-30 15:47:13 +0900

    feat: 연습문제 1 생성자 Test 클래스 선언 후 hp,mp,name 테스트

--- src/test/kotlin/com/survival/kotlinstudy/02_instance_class/ClericTest.kt
+++ src/test/kotlin/com/survival/kotlinstudy/02_instance_class/ClericTest.kt
@@ -9,10 +9,9 @@ class ClericTest {
     @Test
     fun `Cleric 클래스 선언 시 초기 HP MP, 최대 HP MP 확인하기`() {
         val cleric = Cleric(name = "성직자")
-        assertEquals(50,cleric.hp)
-        assertEquals(50,cleric.maxHp)
-        assertEquals(10,cleric.mp)
-        assertEquals(10,cleric.maxMp)
+        assertEquals(50, cleric.hp)
+        assertEquals(10, cleric.mp)
+        assertEquals("성직자",cleric.name)
     }
 
     @Test
@@ -71,7 +70,7 @@ class ClericTest {
     @Test
     fun `pray() 메서드 실행 시 MP를 회복해야 한다`() {
         // given (준비)
-        val cleric = Cleric(name = "성직자",mp = 5)
+        val cleric = Cleric(name = "성직자", mp = 5)
 
         // when (실행)
         cleric.pray(3)
