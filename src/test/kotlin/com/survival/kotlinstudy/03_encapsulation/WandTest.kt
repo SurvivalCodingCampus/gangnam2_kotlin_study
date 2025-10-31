6eaac97b1d849303b1736eb5442596c74448c4d9
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 16:10:47 +0900

    feat: 캡슐화 연습문제 1-2 2. 지팡이의 마력은 0.5 이상 100.0 이하여야 한다 조건 추가 테스트!

--- src/test/kotlin/com/survival/kotlinstudy/03_encapsulation/WandTest.kt
+++ src/test/kotlin/com/survival/kotlinstudy/03_encapsulation/WandTest.kt
@@ -1,8 +1,11 @@
 package com.survival.kotlinstudy.`03_encapsulation`
 
+import com.survival.kotlinstudy.`03_encapsulation`.Wand.Companion.MAX_POWER
+import com.survival.kotlinstudy.`03_encapsulation`.Wand.Companion.MIN_POWER
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
+
 
 class WandTest {
 
@@ -47,6 +50,36 @@ class WandTest {
         }
         // then (검증)
         assertEquals("지팡이의 이름은 null 일 수 없습니다", exception.message)
+    }
+
+    @Test
+    fun `Wand의 마력은 MIN_POWER 이상이어야 한다`() {
+        // given (준비)
+        val name = "사탕 지팡이"
+        val power = 5.0
+        val wand = Wand(name = name, power = power)
+
+        // when (실행)
+        val exception = assertThrows<IllegalArgumentException> {
+            wand.power = MIN_POWER - 1
+        }
+        // then (검증)
+        assertEquals("지팡이의 마력은 $MIN_POWER 이상이어야 한다", exception.message)
+    }
+
+    @Test
+    fun `Wand의 마력은 MAX_POWER 이하이어야 한다`() {
+        // given (준비)
+        val name = "사탕 지팡이"
+        val power = 5.0
+        val wand = Wand(name = name, power = power)
+
+        // when (실행)
+        val exception = assertThrows<IllegalArgumentException> {
+            wand.power = MAX_POWER + 1
+        }
+        // then (검증)
+        assertEquals("지팡이의 마력은 $MAX_POWER 이하여야 한다", exception.message)
     }
 
 }
