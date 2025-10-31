6fd9f8bb790e9a690dc9224ab5558aae64118c32
japygo <hyunnjinn@gmail.com>
2025-10-31 16:12:44 +0900

    feat: 캡슐화 연습문제 1-2

--- src/main/kotlin/com/sesac/practice/day04/Wand.kt
+++ src/main/kotlin/com/sesac/practice/day04/Wand.kt
@@ -1,6 +1,17 @@
 package com.sesac.practice.day04
 
 class Wand(
-    var name: String,
-    var power: Double,
-)
+    val name: String,
+    val power: Double,
+) {
+    init {
+        require(name.length >= NAME_SIZE) { "이름은 ${NAME_SIZE}자 이상이어야 합니다." }
+        require(power in MIN_POWER..MAX_POWER) { "마력은 $MIN_POWER 이상 $MAX_POWER 이하여야 합니다." }
+    }
+
+    companion object {
+        const val NAME_SIZE = 3
+        const val MIN_POWER = 0.5
+        const val MAX_POWER = 100.0
+    }
+}
--- src/main/kotlin/com/sesac/practice/day04/Wizard.kt
+++ src/main/kotlin/com/sesac/practice/day04/Wizard.kt
@@ -1,7 +1,37 @@
 package com.sesac.practice.day04
 
+import kotlin.math.max
+
 class Wizard(
-    var name: String,
-    var hp: Int,
-    var wand: Wand?,
-)
+    val name: String,
+    hp: Int,
+    mp: Int,
+    val wand: Wand?,
+) {
+    var hp = hp
+        set(value) {
+            field = max(value, MIN_HP)
+        }
+
+    var mp = mp
+        set(value) {
+            require(value >= MIN_MP) { "MP는 $MIN_MP 이상이어야 합니다." }
+
+            field = value
+        }
+
+    init {
+        require(name.length >= NAME_SIZE) { "이름은 ${NAME_SIZE}자 이상이어야 합니다." }
+        require(mp >= MIN_MP) { "MP는 $MIN_MP 이상이어야 합니다." }
+
+        if (hp < MIN_HP) {
+            this.hp = MIN_HP
+        }
+    }
+
+    companion object {
+        const val NAME_SIZE = 3
+        const val MIN_HP = 0
+        const val MIN_MP = 0
+    }
+}
