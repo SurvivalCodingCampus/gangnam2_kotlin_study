804522bde57be5a31744ccba802478ccade9143a
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 15:47:03 +0900

    feat: 캡슐화 연습문제 1-2 1. 지팡이의 이름은 null 일 수 없고 반드시 3문자 이상이어야 한다 조건 추가

--- src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wand.kt
+++ src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wand.kt
@@ -1,8 +1,14 @@
 package com.survival.kotlinstudy.`03_encapsulation`
 
 class Wand(
-    var name: String,
+    name: String,
     var power: Double,
 ) {
+    var name: String = name
+        set(value) {
+            require(value.isNotEmpty()) { "지팡이의 이름은 null 일 수 없습니다" }
+            require(value.length >= 3) { "지팡이의 이름은 3글자 이상이어야 합니다" }
+            field = value
+        }
 
 }
