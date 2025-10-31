74c44bdc12aee62ed4bed7353b54f0599dc794c5
Neouul <fluctu0q0@gmail.com>
2025-10-31 16:38:31 +0900

    Feat: 캡슐화 연습문제1-2 타당성 검사

--- src/main/kotlin/com/neouul/sesac/04-encapsulation/Wand.kt
+++ src/main/kotlin/com/neouul/sesac/04-encapsulation/Wand.kt
@@ -1,6 +1,19 @@
 package com.neouul.sesac.`04-encapsulation`
 
 class Wand(
-    var name: String,
-    var power: Double,
-)
+    name: String,
+    power: Double,
+) {
+    var name: String = name
+        set(value) {
+            require(value.length >= MIN_NAME_LENGTH) { "invalid value: 이름은 ${MIN_NAME_LENGTH}자 이상이어야 합니다" }
+            field = value
+        }
+
+    var power: Double = power
+        set(value) {
+            require(value >= MIN_WAND_POWER) { "invalid value: 지팡이의 마력은 ${MIN_WAND_POWER} 이상이어야 합니다" }
+            require(value <= MAX_WAND_POWER) { "invalid value: 지팡이의 마력은 ${MAX_WAND_POWER} 이하이어야 합니다" }
+            field = value
+        }
+}
--- src/main/kotlin/com/neouul/sesac/04-encapsulation/Wizard.kt
+++ src/main/kotlin/com/neouul/sesac/04-encapsulation/Wizard.kt
@@ -1,7 +1,29 @@
 package com.neouul.sesac.`04-encapsulation`
 
 class Wizard(
-    var name: String,
-    var hp: Int,
+    name: String,
+    hp: Int,
     var wand: Wand?,
-)
+) {
+    var name: String = name
+        set(value) {
+            require(value.length >= MIN_NAME_LENGTH) { "invalid value: 이름은 ${MIN_NAME_LENGTH}자 이상이어야 합니다" }
+            field = value
+        }
+
+    var mp: Int = MAX_WIZARD_MP
+        set(value) {
+            require(value >= 0) { "invalid value: MP는 0 이상이어야 합니다" }
+            field = value
+        }
+
+    var hp: Int = hp
+        set(value) {
+            // TODO: 예외를 던져야 할까??
+            field = if (value < 0) 0 else value
+        }
+
+    fun fireball(){
+
+    }
+}
