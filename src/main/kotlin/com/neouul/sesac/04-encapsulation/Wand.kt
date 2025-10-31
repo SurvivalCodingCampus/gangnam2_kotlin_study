5c9a82c078e13bad8dbf6429cbda5c2c94694512
Neouul <fluctu0q0@gmail.com>
2025-10-31 15:14:55 +0900

    Feat: 연습문제 1-1 클래스 정의

---
+++ src/main/kotlin/com/neouul/sesac/04-encapsulation/Wand.kt
@@ -0,0 +1,6 @@
+package com.neouul.sesac.`04-encapsulation`
+
+class Wand(
+    var name: String,
+    var power: Double,
+)
---
+++ src/main/kotlin/com/neouul/sesac/04-encapsulation/Wizard.kt
@@ -0,0 +1,7 @@
+package com.neouul.sesac.`04-encapsulation`
+
+class Wizard(
+    var name: String,
+    var hp: Int,
+    var wand: Wand?,
+)
