5edc9e0f0e49c1c4b0daec5305bf8818372e3262
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 16:17:47 +0900

    feat: 캡슐화 연습문제 1-2 3.마법사의 MP는 0 이상이어야 한다 조건 추가

--- src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wizard.kt
+++ src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wizard.kt
@@ -3,12 +3,18 @@ package com.survival.kotlinstudy.`03_encapsulation`
 class Wizard(
     name: String,
     var hp: Int,
-    var wand: Wand?
+    var wand: Wand?,
 ) {
     var name: String = name
         set(value) {
             require(value.isNotEmpty()) { "마법사의 이름은 null 일 수 없습니다" }
             require(value.length >= 3) { "마법사의 이름은 3글자 이상이어야 합니다" }
+            field = value
+        }
+
+    var mp: Int = 10
+        set(value) {
+            require(value >= 0) { "마법사의 MP 는 0 이상이어야 합니다" }
             field = value
         }
 }
