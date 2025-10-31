a23719b1ed2ea470478566a721bd88e7c764e8eb
Neouul <fluctu0q0@gmail.com>
2025-10-31 11:08:41 +0900

    Test: 연습문제 2-B 만족하는 경우

--- src/test/kotlin/com/neouul/sesac/03-class/ClericTest.kt
+++ src/test/kotlin/com/neouul/sesac/03-class/ClericTest.kt
@@ -17,13 +17,11 @@ class ClericTest {
 
     @Test
     fun `Cleric 생성자가 연습문제 2-B를 만족하는지`(){
-        // given
         val cleric = Cleric(name = "아서스", hp = 35)
 
-        // when
-
-        // then
-
+        assertEquals("아서스", cleric.name)
+        assertEquals(35, cleric.hp)
+        assertEquals(Cleric.MAX_MP, cleric.mp)
     }
   
 }
