8663a7f165ee2e877b52305759378c0c1e21f0ef
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 16:10:24 +0900

    feat: 캡슐화 연습문제 1-2 2. 지팡이의 마력은 0.5 이상 100.0 이하여야 한다 조건 추가

--- src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wand.kt
+++ src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Wand.kt
@@ -2,7 +2,7 @@ package com.survival.kotlinstudy.`03_encapsulation`
 
 class Wand(
     name: String,
-    var power: Double,
+    power: Double,
 ) {
     var name: String = name
         set(value) {
@@ -10,5 +10,19 @@ class Wand(
             require(value.length >= 3) { "지팡이의 이름은 3글자 이상이어야 합니다" }
             field = value
         }
+
+    var power: Double = power
+        set(value) {
+            require(value >= MIN_POWER) {"지팡이의 마력은 ${MIN_POWER} 이상이어야 한다"}
+            require(value <= MAX_POWER) {"지팡이의 마력은 ${MAX_POWER} 이하여야 한다"}
+            field = value
+        }
+
+
+    companion object {
+        const val MIN_POWER = 0.5
+        const val MAX_POWER = 100.0
+    }
+
 
 }
