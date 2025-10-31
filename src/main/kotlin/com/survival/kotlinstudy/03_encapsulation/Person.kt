27aaf1af9a513063eda5afa46ace172fd6f1e39b
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 17:03:36 +0900

    feat: 캡슐화 연습문제 2 Person 클래스

--- src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Person.kt
+++ src/main/kotlin/com/survival/kotlinstudy/03_encapsulation/Person.kt
@@ -4,7 +4,7 @@ import java.time.LocalDate
 
 class Person(
     val name: String,
-    val birthYear: Int
+    val birthYear: Int,
 ) {
     val age: Int
         get() = LocalDate.now().year - birthYear
