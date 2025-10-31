93c7750187814714168661ca6ba01a858afb3e2d
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 15:47:07 +0900

    feat: 캡슐화 연습문제 1-2 1. 지팡이의 이름은 null 일 수 없고 반드시 3문자 이상이어야 한다 조건 테스트

--- src/test/kotlin/com/survival/kotlinstudy/03_encapsulation/WandTest.kt
+++ src/test/kotlin/com/survival/kotlinstudy/03_encapsulation/WandTest.kt
@@ -1,6 +1,7 @@
 package com.survival.kotlinstudy.`03_encapsulation`
 
 import org.junit.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class WandTest {
@@ -17,6 +18,35 @@ class WandTest {
         // then (검증)
         assertEquals(name, wand.name)
         assertEquals(power, wand.power)
+    }
+
+    @Test
+    fun `Wand의 이름이 3글자 미만이면 예외가 발생한다`() {
+        val name = "사탕 지팡이"
+        val power = 5.0
+        // given (준비)
+        val wand = Wand(name = name, power = power)
+        // when (실행)
+        val exception = assertThrows<IllegalArgumentException> {
+            wand.name = "사탕"
+        }
+        // then (검증)
+        assertEquals("지팡이의 이름은 3글자 이상이어야 합니다", exception.message)
+    }
+
+    @Test
+    fun `Wand의 이름이 null 이면 예외가 발생한다`() {
+        // given (준비)
+        val name = "사탕 지팡이"
+        val power = 5.0
+        val wand = Wand(name = name, power = power)
+
+        // when (실행)
+        val exception = assertThrows<IllegalArgumentException> {
+            wand.name = ""
+        }
+        // then (검증)
+        assertEquals("지팡이의 이름은 null 일 수 없습니다", exception.message)
     }
 
 }
