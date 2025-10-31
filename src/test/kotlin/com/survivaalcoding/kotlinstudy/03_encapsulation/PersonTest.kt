69840460a2a7302865192338be96deedba116463
whoamixzerone <whoamixzerone@gmail.com>
2025-10-31 17:46:34 +0900

    feat: 캡슐화 연습문제 2 완료

---
+++ src/test/kotlin/com/survivaalcoding/kotlinstudy/03_encapsulation/PersonTest.kt
@@ -0,0 +1,35 @@
+package com.survivaalcoding.kotlinstudy.`03_encapsulation`
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.time.LocalDate
+
+class PersonTest {
+    @Test
+    fun `Person 클래스 인스턴스 생성 확인`() {
+        // given
+        val name = "홍길동"
+        val birthYear = 2000
+
+        // when
+        val person = Person(name, birthYear)
+
+        // then
+        assertThat(person.name).isEqualTo(name)
+        assertThat(person.birthYear).isEqualTo(birthYear)
+    }
+
+    @Test
+    fun `2000년 생의 나이는 25세다`() {
+        // given
+        val name = "홍길동"
+        val birthYear = 2000
+        val age = LocalDate.now().year - birthYear
+
+        // when
+        val person = Person(name, birthYear)
+
+        // then
+        assertThat(person.age).isEqualTo(age)
+    }
+}
