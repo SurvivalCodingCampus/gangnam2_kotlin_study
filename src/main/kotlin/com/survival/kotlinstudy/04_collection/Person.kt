115f9cdf35a455a296b6ce5be956928aba39dec9
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-10-31 17:20:55 +0900

    feat: 컬렉션 연습문제 3

--- src/main/kotlin/com/survival/kotlinstudy/04_collection/Person.kt
+++ src/main/kotlin/com/survival/kotlinstudy/04_collection/Person.kt
@@ -3,15 +3,27 @@ package com.survival.kotlinstudy.`04_collection`
 class Person(
     val name: String,
 ) {
-
-
+    var age: Int = 0
 }
 
 fun main() {
+    // 연습문제 2
     val hong = Person(name = "홍길동")
     val han = Person(name = "한석봉")
     val list = listOf(hong, han)
     for (person in list) {
         println(person.name)
     }
+
+    // 연습문제 3
+    hong.age = 20
+    han.age = 25
+
+    val map = mapOf(hong.name to hong.age, han.name to han.age)
+
+    for ((name, age) in map) {
+        println("${name}의 나이는 ${age}살")
+    }
+
+
 }
