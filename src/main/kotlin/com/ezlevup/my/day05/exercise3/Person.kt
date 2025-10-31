aa59af57ced7a6592bd5a37d82fa09b92d21776e
이영현 <hong.git@yahoo.com>
2025-11-01 01:14:44 +0900

    feat: 컬렉션 연습문제 3

--- src/main/kotlin/com/ezlevup/my/day05/exercise3/Person.kt
+++ src/main/kotlin/com/ezlevup/my/day05/exercise3/Person.kt
@@ -1,7 +1,7 @@
 package com.ezlevup.my.day05.exercise3
 
 class Person(
-    val name: String
+    val name: String,
 ) {}
 
 fun main() {
@@ -9,5 +9,14 @@ fun main() {
     persons.add(Person(name = "홍길동"))
     persons.add(Person(name = "한석봉"))
 
+    // 연습문제 2
     persons.forEach { person -> println(person.name) }
+
+    // 연습문제 3
+    val persons2 = mutableMapOf<Person, Int>()
+    persons2[Person(name = "홍길동")] = 20
+    persons2[Person(name = "한석봉")] = 25
+
+    persons2.forEach { p -> println("${p.key.name}의 나이는 ${p.value}살") }
+
 }
