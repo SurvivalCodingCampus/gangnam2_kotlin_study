ce7fd9f47e0a15554899a338a4d853c5fc4eaac5
MisterJerry <misterjerry12345@gmail.com>
2025-10-31 16:51:25 +0900

    feat: 연습문제 1-2, 인스턴스 생성 규칙 위반시 프로그램 터트리는 기능 추가 및 매직넘버 반영

--- src/main/kotlin/_251031_kotlin_oop/encapsulation/Wand.kt
+++ src/main/kotlin/_251031_kotlin_oop/encapsulation/Wand.kt
@@ -1,8 +1,20 @@
 package _251031_kotlin_oop.encapsulation
 
+const val MINIMUM_NAME_LENGTH = 3
+
+const val WAND_MP_MIN = 0.5
+const val WAND_MP_MAX = 100.0
+
 class Wand(
     var name: String,
     var power: Double,
-    
-    ) {
+) {
+    init {
+        if (name.isEmpty() || name.length < MINIMUM_NAME_LENGTH) {
+            throw IllegalArgumentException("지팡이의 이름은 ${MINIMUM_NAME_LENGTH}글자 이상이어야 합니다.")
+        }
+        if (power !in WAND_MP_MIN..WAND_MP_MAX) {
+            throw IllegalArgumentException("지팡이의 마력은 ${WAND_MP_MIN}이상 ${WAND_MP_MAX}이하여야 합니다.")
+        }
+    }
 }
--- src/main/kotlin/_251031_kotlin_oop/encapsulation/Wizard.kt
+++ src/main/kotlin/_251031_kotlin_oop/encapsulation/Wizard.kt
@@ -1,8 +1,29 @@
 package _251031_kotlin_oop.encapsulation
 
+const val WIZARD_MINIMUM_MP = 0.0
+
 class Wizard(
     var name: String,
     var hp: Int,
     var wand: Wand?,
 ) {
+    init {
+        if (name.isEmpty() || name.length < MINIMUM_NAME_LENGTH) {
+            throw IllegalArgumentException("마법사의 이름은 3글자 이상이어야 합니다.")
+        }
+        val power = wand?.power ?: 0.0
+        if (power < WIZARD_MINIMUM_MP) {
+            throw IllegalArgumentException("마법사의 MP는 ${WIZARD_MINIMUM_MP}이상이어야 합니다.")
+        }
+
+    }
+
+    fun attacked(damage: Int) {//공격 받은것
+        if (hp - damage < 0) {
+            hp = 0
+        } else {
+            hp -= damage
+        }
+    }
+
 }
