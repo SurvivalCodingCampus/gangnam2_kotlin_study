0a53912356598594141b36a215af37d800682963
Neouul <fluctu0q0@gmail.com>
2025-11-03 14:24:46 +0900

    Feat: Slime 클래스 정의

--- src/main/kotlin/com/neouul/sesac/05-inheritance/Slime.kt
+++ src/main/kotlin/com/neouul/sesac/05-inheritance/Slime.kt
@@ -1,4 +1,14 @@
 package com.neouul.sesac.`05-inheritance`
 
-class Slime(var hp: Int) {
+class Slime(val suffix: String) {
+    companion object{
+        const val DAMAGE = 10
+    }
+    var hp = 50
+
+    fun attack(hero: Hero){
+        println("슬라임 $suffix 가 공격했다")
+        println("${DAMAGE}의 데미지")
+        hero.hp -= DAMAGE
+    }
 }
