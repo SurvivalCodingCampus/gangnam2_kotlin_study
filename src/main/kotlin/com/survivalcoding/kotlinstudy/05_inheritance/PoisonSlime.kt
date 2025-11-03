e75a61eb3336499a69b31e9dc5ba50f6271ffa6b
Jieun Lee <geniee1750@gmail.com>
2025-11-03 18:47:50 +0900

    [과제] test: poisonSlime 테스트 코드 작성

--- src/main/kotlin/com/survivalcoding/kotlinstudy/05_inheritance/PoisonSlime.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/05_inheritance/PoisonSlime.kt
@@ -4,7 +4,19 @@ package com.survivalcoding.kotlinstudy.`05_inheritance`
 class PoisonSlime(
     suffix: String
 ) : Slime(suffix) {
-    var poisonCount = 5
+    // 테스트를 위해서 매직 넘버 제거
+    companion object {
+        const val INITIAL_POISON_COUNT = 5
+        const val DAMAGE_RATIO = 5   // 최대 HP의 1/5
+    }
+
+    // 캡슐화 -> get 만 가능하게 함
+    var poisonCount = INITIAL_POISON_COUNT
+        private set
+
+    var lastDamage: Int = 0
+        private set
+
 
     override fun attack(hero: Hero) {
         // 보통 슬라임과 같은 공격
@@ -13,10 +25,12 @@ class PoisonSlime(
         // poisonCount가 0이 아니면 추가 공격
         if (poisonCount > 0) {
             println("추가로, 독 포자를 살포했다!")
-            val damage = hero.maxHp / 5  // 용사의 최대 HP의 1/5 만큼 데미지
-            hero.hp -= damage
-            println("${damage} 포인트의 데미지!")
-            poisonCount -= 1  // poisonCount 1 감소
+            lastDamage = hero.maxHp / DAMAGE_RATIO   // 용사의 최대 HP의 1/5 만큼 데미지
+            hero.hp -= lastDamage
+            println("${lastDamage} 포인트의 데미지!")
+            poisonCount -= 1    // poisonCount 1 감소
+        } else {
+            lastDamage = 0  // 독 공격이 발생하지 않음
         }
     }
 }
