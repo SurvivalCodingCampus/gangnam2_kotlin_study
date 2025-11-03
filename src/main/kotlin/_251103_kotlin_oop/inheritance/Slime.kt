99f38ac7f1dccdd4b79b7d7f5a2cb58bbad3dd2c
MisterJerry <misterjerry12345@gmail.com>
2025-11-03 15:20:01 +0900

    chore: 매직넘버 지우는 작업 시행

--- src/main/kotlin/_251103_kotlin_oop/inheritance/Slime.kt
+++ src/main/kotlin/_251103_kotlin_oop/inheritance/Slime.kt
@@ -1,12 +1,15 @@
 package _251103_kotlin_oop.inheritance
 
+const val SLIMEBASICATTCK = 10
+const val SLIMEBASICHP = 50
+
 open class Slime(
     val suffix: String
 ) {
-    var hp = 50
+    var hp = SLIMEBASICHP
     open fun attack(hero: Hero) {
         println("슬라임 $suffix 가 공격했다")
-        println("10의 데미지")
-        hero.hp -= 10
+        println("${SLIMEBASICATTCK}의 데미지")
+        hero.hp -= SLIMEBASICATTCK
     }
 }
