5c82bbe1fbd1f6036d63c5addf54f030ee297ed2
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-11-03 12:00:39 +0900

    feat: 과제 리뷰

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/04_collection/Epsillon.kt
@@ -0,0 +1,12 @@
+package com.survivalcoding.kotlinstudy.`04_collection`
+
+fun main() {
+    val a: Double = 0.1
+    val b = 0.2
+
+    val c = a + b
+
+    println("       ".isEmpty())
+    println(c)
+    println(c == 0.3)
+}
