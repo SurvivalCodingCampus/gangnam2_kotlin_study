b460d0e9a4cf3d58c4cf46eaed36be7a75273066
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2025-11-03 10:31:50 +0900

    Refactor: 나이 계산 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/03_encapsulation/practice/Person.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/03_encapsulation/practice/Person.kt
@@ -6,10 +6,7 @@ class Person(
     val name: String,   // 이름
     val birthYear: Int, // 태어난 해
 ) {
-    // 올해 년도
-    private val currentYear: Int = LocalDate.now().year
-
     // 나이
     val age: Int
-        get() = currentYear - birthYear
+        get() = LocalDate.now().year - birthYear
 }
