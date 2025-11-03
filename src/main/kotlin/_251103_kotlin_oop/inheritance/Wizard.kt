226fd4a6e6fc08e13f7f92d5ccd970a5abe479a1
MisterJerry <misterjerry12345@gmail.com>
2025-11-03 16:18:33 +0900

    feat: 연습문제4, 이전에 작성한 Wizard 클래스 수정 및 테스트코드 작성, 매직넘버 처리

--- src/main/kotlin/_251103_kotlin_oop/inheritance/Wizard.kt
+++ src/main/kotlin/_251103_kotlin_oop/inheritance/Wizard.kt
@@ -1,9 +1,12 @@
 package _251103_kotlin_oop.inheritance
 
+import _251030_kotlin_oop.MAX_HP
 import _251031_kotlin_oop.encapsulation.MINIMUM_NAME_LENGTH
 import _251031_kotlin_oop.encapsulation.Wand
 
-const val WIZARD_MINIMUM_MP = 100
+const val WIZARD_MINIMUM_MP = 100 //마법사 최소 mp
+const val HEALREQUIREDMP = 10 //heal 메서드 필요 mp
+const val HEALTREATHP = 20 //heal 메서드가 회복시키는 hp
 
 class Wizard(
     var name: String,
@@ -27,6 +30,20 @@ class Wizard(
             hp = 0
         } else {
             hp -= damage
+        }
+    }
+
+    fun heal(hero: Hero) {
+        if (mp >= HEALREQUIREDMP) {
+            mp -= HEALREQUIREDMP
+            if (hero.hp + HEALTREATHP > MAX_HP) {
+                hero.hp = MAX_HP
+            } else {
+                hero.hp += HEALTREATHP
+            }
+            println("힐을 시전했습니다. 대상 HP: ${hero.hp}")
+        } else {
+            println("마나가 부족합니다.")
         }
     }
 
