dbebc41daa24ee049940976ce46a77e99c7e1139
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-03 14:11:48 +0900

    feat: 상속 연습문제 3

---
+++ src/main/kotlin/com/survival/kotlinstudy/05_inheritance/PoisonSlime.kt
@@ -0,0 +1,30 @@
+package com.survival.kotlinstudy.`05_inheritance`
+
+import com.survival.kotlinstudy.`02_instance_class`.Hero
+
+
+class PoisonSlime(
+    suffix: String
+) : Slime(suffix) {
+    var poisonCount = 5
+
+    override fun attack(hero: Hero) {
+        super.attack(hero)
+
+        if (poisonCount > 0) {
+            println("추가로, 독 포자를 살포했다!")
+            val damage = hero.hp / 5
+            hero.hp -= damage
+            println("$damage 포인트의 데미지")
+            poisonCount--
+        }
+    }
+}
+
+fun main() {
+    val poisonSlime = PoisonSlime("A")
+    val hero = Hero(name = "홍길동", hp = 100)
+
+    poisonSlime.attack(hero)
+
+}
