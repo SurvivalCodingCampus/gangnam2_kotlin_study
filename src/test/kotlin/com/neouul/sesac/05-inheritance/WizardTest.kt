1f592e6cb9d4bd66fc3605a80899def7c8693a40
Neouul <fluctu0q0@gmail.com>
2025-11-03 19:44:11 +0900

    Chore: 코드 정리

--- src/test/kotlin/com/neouul/sesac/05-inheritance/WizardTest.kt
+++ src/test/kotlin/com/neouul/sesac/05-inheritance/WizardTest.kt
@@ -5,7 +5,7 @@ import org.junit.Test
 
 class WizardTest {
     @Test
-    fun `Wizard의 인스턴스가 성공적으로 생성된 경우, mp의 초기값이 올바른지`(){
+    fun `Wizard의 인스턴스가 성공적으로 생성된 경우, mp의 초기값이 올바른지`() {
         // given
         val wizard = Wizard("마법사", 50, null)
 
@@ -17,7 +17,7 @@ class WizardTest {
     }
 
     @Test
-    fun `Wizard의 heal 메서드가 용사의 hp와 자신의 mp를 적절히 조절하는 경우`(){
+    fun `Wizard의 heal 메서드가 용사의 hp와 자신의 mp를 적절히 조절하는 경우`() {
         // given: 용사의 최대 HP인 80보다 20이상 적은 값으로 설정하여
         // 회복이 예상하는 값 만큼 되었는지 확인
         val wizard = Wizard("마법사", 50, null)
@@ -34,7 +34,7 @@ class WizardTest {
     }
 
     @Test
-    fun `Wizard의 heal 메서드를 호출했는데 mp가 부족한 경우`(){
+    fun `Wizard의 heal 메서드를 호출했는데 mp가 부족한 경우`() {
         // given: 마법사의 mp를 heal 사용하는 mp보다 적게 설정하여 마나가 부족한 경우를 테스트
         val wizard = Wizard("마법사", 50, null)
         val hero = Hero("용사", 10)
