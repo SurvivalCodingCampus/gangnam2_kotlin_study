cd901e83caf6660e975c9df08d6692e268b32020
MisterJerry <misterjerry12345@gmail.com>
2025-11-03 15:27:26 +0900

    fix: 설계 잘못한 코드 수정 (지팡이의 소비 mp를 마법사의 mp로 오해하여 설계함)

--- src/main/kotlin/_251103_kotlin_oop/inheritance/Wizard.kt
+++ src/main/kotlin/_251103_kotlin_oop/inheritance/Wizard.kt
@@ -3,19 +3,20 @@ package _251103_kotlin_oop.inheritance
 import _251031_kotlin_oop.encapsulation.MINIMUM_NAME_LENGTH
 import _251031_kotlin_oop.encapsulation.Wand
 
-const val WIZARD_MINIMUM_MP = 0.0
+const val WIZARD_MINIMUM_MP = 100
 
 class Wizard(
     var name: String,
     var hp: Int,
     var wand: Wand?,
 ) {
+    var mp: Int = WIZARD_MINIMUM_MP
+
     init {
         if (name.isEmpty() || name.length < MINIMUM_NAME_LENGTH) {
             throw IllegalArgumentException("마법사의 이름은 3글자 이상이어야 합니다.")
         }
-        val power = wand?.power ?: 0.0
-        if (power < WIZARD_MINIMUM_MP) {
+        if (mp < WIZARD_MINIMUM_MP) {
             throw IllegalArgumentException("마법사의 MP는 ${WIZARD_MINIMUM_MP}이상이어야 합니다.")
         }
 
