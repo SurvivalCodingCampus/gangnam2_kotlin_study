d3be09049960802590ee540ef447209d154f68ba
Neouul <fluctu0q0@gmail.com>
2025-11-03 14:43:10 +0900

    Feat: 연습문제 3 PoisonSlime 구현

---
+++ src/main/kotlin/com/neouul/sesac/05-inheritance/PoisonSlime.kt
@@ -0,0 +1,19 @@
+package com.neouul.sesac.`05-inheritance`
+
+class PoisonSlime(
+    suffix: String,
+) : Slime(suffix) {
+    var poisonCount = 5
+
+    override fun attack(hero: Hero) {
+        super.attack(hero)
+
+        if(poisonCount != 0){
+            println("추가로, 슬라임 $suffix 가 독 포자를 살포했다!")
+            val poisonDamage = hero.hp / 5
+            hero.hp -= poisonDamage
+            println("$poisonDamage 포인트의 데미지")
+            poisonCount--
+        }
+    }
+}
--- src/main/kotlin/com/neouul/sesac/05-inheritance/Slime.kt
+++ src/main/kotlin/com/neouul/sesac/05-inheritance/Slime.kt
@@ -1,12 +1,12 @@
 package com.neouul.sesac.`05-inheritance`
 
-class Slime(val suffix: String) {
+open class Slime(val suffix: String) {
     companion object{
         const val DAMAGE = 10
     }
     var hp = 50
 
-    fun attack(hero: Hero){
+    open fun attack(hero: Hero){
         println("슬라임 $suffix 가 공격했다")
         println("$DAMAGE 의 데미지")
         hero.hp -= DAMAGE
