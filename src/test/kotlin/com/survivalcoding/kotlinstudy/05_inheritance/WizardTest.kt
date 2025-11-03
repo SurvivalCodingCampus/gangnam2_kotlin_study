19c076dde7b26d6ec30d133bfffbb709e07a8e2a
Jieun Lee <geniee1750@gmail.com>
2025-11-03 19:01:53 +0900

    [과제] test: Wizard 테스트 코드 작성

---
+++ src/test/kotlin/com/survivalcoding/kotlinstudy/05_inheritance/WizardTest.kt
@@ -0,0 +1,88 @@
+package com.survivalcoding.kotlinstudy.`05_inheritance`
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class WizardTest {
+
+    companion object {
+        // Hero
+        private const val HERO_NAME = "용사"
+        private const val HERO_INITIAL_HP = 50
+        private const val NEGATIVE_HP = -10
+
+        // Wizard
+        private const val WIZARD_NAME = "마법사"
+        private const val WIZARD_MP_FULL = 100
+        private const val WIZARD_MP_LOW = 5
+        private const val WIZARD_HP = 80
+        private const val WIZARD_HP_LOW = 70
+        private const val WIZARD_HP_FULL = 90
+
+        // Wand
+        private const val WAND_NAME = "지팡이"
+        private const val WAND_POWER = 50.0
+
+        // 마나
+        private const val HP_RECOVERY = 20
+        private const val MP_COST = 10
+    }
+
+    @Test
+    fun `힐 성공 - 영웅 HP 20 회복, 마법사 MP 10 소모`() {
+        // given
+        val hero = Hero(HERO_NAME, HERO_INITIAL_HP)
+        val wand = Wand(WAND_NAME, WAND_POWER)
+        val wizard = Wizard(WIZARD_NAME, mp = WIZARD_MP_FULL, hp = WIZARD_HP, wand = wand)
+
+        // when
+        wizard.heal(hero)
+
+        // then
+        assertEquals(HERO_INITIAL_HP + HP_RECOVERY, hero.hp) // 회복 성공
+        assertEquals(WIZARD_MP_FULL - MP_COST, wizard.mp)     // MP 소모 확인
+    }
+
+    @Test
+    fun `마나 부족 - 힐 실패`() {
+        // given
+        val hero = Hero(HERO_NAME, HERO_INITIAL_HP)
+        val wand = Wand(WAND_NAME, WAND_POWER)
+        val wizard = Wizard(WIZARD_NAME, mp = WIZARD_MP_LOW, hp = WIZARD_HP_LOW, wand = wand)
+
+        // when
+        wizard.heal(hero)
+
+        // then
+        assertEquals(HERO_INITIAL_HP, hero.hp)  // 회복되지 않음
+        assertEquals(WIZARD_MP_LOW, wizard.mp)  // MP 소모 없음
+    }
+
+    @Test
+    fun `회복 후 MP가 0이 되면 다음 힐은 실패한다`() {
+        // given
+        val hero = Hero(HERO_NAME, HERO_INITIAL_HP)
+        val wand = Wand(WAND_NAME, WAND_POWER)
+        val wizard = Wizard(WIZARD_NAME, mp = MP_COST, hp = WIZARD_HP_FULL, wand = wand)
+
+        // when
+        wizard.heal(hero)  // 첫 힐 성공
+        wizard.heal(hero)  // 두 번째 힐 시도 (MP 부족)
+
+        // then
+        assertEquals(HERO_INITIAL_HP + HP_RECOVERY, hero.hp) // 첫 힐만 성공
+        assertEquals(0, wizard.mp)                           // MP 전부 소모됨
+    }
+
+    @Test
+    fun `HP가 0 이하인 경우 - 음수 X`() {
+        // given
+        val hero = Hero(HERO_NAME, NEGATIVE_HP)
+        val wand = Wand(WAND_NAME, WAND_POWER)
+        val wizard = Wizard(WIZARD_NAME, hp = WIZARD_HP, wand = wand)
+
+        // then
+        assertEquals(0, hero.hp)                 // Hero에서 0으로 보정되어야 함
+        assertEquals(WIZARD_HP, wizard.hp)
+    }
+}
