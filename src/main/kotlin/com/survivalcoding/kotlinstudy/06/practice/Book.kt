2c60b50a4ddb27ae882336334ac26bcacecfe1bd
wantfree8937 <qkrtmdduq112@naver.com>
2025-11-04 17:58:16 +0900

    docs: 25/11/04 TIL

--- src/main/kotlin/com/survivalcoding/kotlinstudy/06/practice/Book.kt
+++
@@ -1,9 +0,0 @@
-package com.survivalcoding.kotlinstudy.`06`.practice
-
-class Book(
-    name: String,
-    price: Int,
-    color: String,
-    var isbn: String
-) : TangibleAsset(name, price, color) {
-}
--- src/main/kotlin/com/survivalcoding/kotlinstudy/06/practice/Computer.kt
+++
@@ -1,9 +0,0 @@
-package com.survivalcoding.kotlinstudy.`06`.practice
-
-class Computer(
-    name: String,
-    price: Int,
-    color: String,
-    var makerName: String
-) : TangibleAsset(name, price, color) {
-}
