a00c0259de7a6b93a31fc4481830c59a90df6a80
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-04 14:25:51 +0900

    feat: 연습문제 1 인스턴스 생성 테스트

---
+++ src/test/kotlin/com/survival/kotlinstudy/06_interface/BookTest.kt
@@ -0,0 +1,26 @@
+package com.survival.kotlinstudy.`06_interface`
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class BookTest {
+
+    @Test
+    fun `Book 인스턴스 생성 테스트`() {
+
+        // given (준비)
+        val name = "책"
+        val price = 10000
+        val color = "Black"
+        val isbn = "123123"
+        val book = Book(name = name ,price = price, color = color, isbn = isbn)
+
+        // when (실행)
+
+        // then (검증)
+        assertEquals(name,book.name)
+        assertEquals(price,book.price)
+        assertEquals(color,book.color)
+        assertEquals(isbn,book.isbn)
+    }
+}
---
+++ src/test/kotlin/com/survival/kotlinstudy/06_interface/ComputerTest.kt
@@ -0,0 +1,26 @@
+package com.survival.kotlinstudy.`06_interface`
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ComputerTest {
+
+    @Test
+    fun `Computer 인스턴스 생성 테스트`() {
+
+        // given (준비)
+        val name = "책"
+        val price = 10000
+        val color = "Black"
+        val markerName = "markerName"
+        val computer = Computer(name = name ,price = price, color = color, markerName = markerName)
+
+        // when (실행)
+
+        // then (검증)
+        assertEquals(name,computer.name)
+        assertEquals(price,computer.price)
+        assertEquals(color,computer.color)
+        assertEquals(markerName,computer.markerName)
+    }
+}
