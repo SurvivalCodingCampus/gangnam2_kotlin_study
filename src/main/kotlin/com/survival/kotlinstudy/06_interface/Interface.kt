002523f9d4b6aca96dbed0293eff8802b1d37ea5
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-04 12:21:26 +0900

    feat: 추상화와 인터페이스

---
+++ src/main/kotlin/com/survival/kotlinstudy/06_interface/Interface.kt
@@ -0,0 +1,16 @@
+package com.survival.kotlinstudy.`06_interface`
+
+//abstract class Human {
+//    abstract fun speak()
+//}
+
+interface Human {
+    fun speak()
+}
+
+class Man : Human {
+    override fun speak() {
+
+    }
+
+}
