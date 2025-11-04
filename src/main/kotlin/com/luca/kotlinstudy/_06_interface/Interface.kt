191fa2dd113c85165c3caa8c0bb171b5a5f72278
unknown <hr.jeon28@gmail.com>
2025-11-04 15:03:24 +0900

    feat: 인터페이스 구현 및 lateinit·lazy 초기화 예제 추가

--- src/main/kotlin/com/luca/kotlinstudy/_06_interface/Interface.kt
+++ src/main/kotlin/com/luca/kotlinstudy/_06_interface/Interface.kt
@@ -1,10 +1,42 @@
 package com.luca.kotlinstudy._06_interface
 
-// 인스턴스화 못함
-Interface Human{
+//abstract class Human {
+//    abstract fun speak()
+//}
+
+// 인스턴스화 못 함
+interface Human {
+    val hp: Int
+
     fun speak()
 }
 
-class Man : Human {
-    fun speak()
+//class Man : Human {
+//    override fun speak() {
+//        TODO("Not yet implemented")
+//    }
+//}
+
+class Person {
+    // 나중에 초기화. 금지 (왠만하면 안 쓴다)
+    lateinit var name: String
+
+    // 인스턴스화 되면서 초기화
+    val age2 = 0
+
+    // 나중에 초기화
+    val age: Int by lazy {
+        0
+    }
+
+    fun function() {
+        name = "홍길동"
+    }
+}
+
+fun main() {
+    var person: Person? = Person()
+    println(person?.name)
+
+    person = null
 }
