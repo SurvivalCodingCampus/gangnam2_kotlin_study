a95da7a5b293ab86069e6eec4963efa91c61ef9b
Jieun Lee <geniee1750@gmail.com>
2025-11-10 14:04:02 +0900

    [과제] feat: 연습문제 1 제출

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/07_polymorphism/Exercise.kt
@@ -0,0 +1,13 @@
+package com.survivalcoding.kotlinstudy.`07_polymorphism`
+
+/**
+ * 연습문제 1. 적절한 클래스 명
+ * (1)
+ * Item
+ * Item, Sword
+ *
+ * (2)
+ * val a: Monster = Slime()
+ * Monster
+ * Slime
+ */
