2713809c16cb10b9861ef4130275b6149852ee24
Jieun Lee <geniee1750@gmail.com>
2025-11-10 14:11:40 +0900

    [과제] feat: 연습문제 2 제출

--- src/main/kotlin/com/survivalcoding/kotlinstudy/07_polymorphism/Exercise.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/07_polymorphism/Exercise.kt
@@ -11,3 +11,56 @@ package com.survivalcoding.kotlinstudy.`07_polymorphism`
  * Monster
  * Slime
  */
+
+/**
+ * 연습문제 2.
+ * 1. a()
+ * 2. Aa, Ba
+ */
+
+interface X {
+    fun a()
+}
+
+abstract class Y : X {
+    abstract fun b()
+}
+
+class A : Y() {
+    override fun b() {
+        println("Ab")
+    }
+
+    override fun a() {
+        println("Aa")
+    }
+
+    fun c() {
+        println("Ac")
+    }
+}
+
+class B : Y() {
+    override fun b() {
+        println("Bb")
+    }
+
+    override fun a() {
+        println("Ba")
+    }
+
+    fun c() {
+        println("Bc")
+    }
+}
+
+fun main() {
+    val obj: X = A()
+    obj.a()
+
+    val y1: Y = A()
+    val y2: Y = B()
+
+    y1.a()
+    y2.a()
+}
