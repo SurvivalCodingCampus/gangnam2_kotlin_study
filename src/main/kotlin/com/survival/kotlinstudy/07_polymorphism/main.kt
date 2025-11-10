21966af66cdb2dad5563a305c6e3e1a3f654dad2
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-10 19:51:57 +0900

    feat: 연습문제 3번

---
+++ src/main/kotlin/com/survival/kotlinstudy/07_polymorphism/main.kt
@@ -0,0 +1,21 @@
+package com.survival.kotlinstudy.`07_polymorphism`
+
+fun main() {
+    val obj: X = A()
+    obj.a()
+
+    val y1 = A()
+    val y2 = B()
+
+    y1.a()
+    y2.a()
+
+    val list = mutableListOf<Y>()
+    list.add(y1)
+    list.add(y2)
+
+    list.forEach {
+        it.b()
+    }
+
+}
