0d62751cc52a7e95c54014bade0ef141b5f4dcaa
wantfree8937 <101966192+wantfree8937@users.noreply.github.com>
2025-11-11 21:52:19 +0900

    refactor: 안 쓰이는 부분 주석 처리
    Commented out the author hash code calculation in hashCode method.

--- src/main/kotlin/com/survivalcoding/kotlinstudy/08_instance_basic/practice/Book.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/08_instance_basic/practice/Book.kt
@@ -44,7 +44,7 @@ class Book(
 ) {
     override fun hashCode(): Int {
         var result = title.hashCode()
-        result = 31 * result + author.hashCode()
+//      result = 31 * result + author.hashCode()
         result = 31 * result + publishedDate.hashCode()
         return result
     }
