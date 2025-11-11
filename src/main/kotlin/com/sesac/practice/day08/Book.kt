e186e9e7b6ff81c3761d48b1b401e3518fc27ecd
japygo <hyunnjinn@gmail.com>
2025-11-11 17:17:55 +0900

    feat: 연습문제

---
+++ src/main/kotlin/com/sesac/practice/day08/Book.kt
@@ -0,0 +1,54 @@
+package com.sesac.practice.day08
+
+import java.time.LocalDateTime
+
+class Book(
+    val title: String,
+    val author: String,
+    val publishedDate: LocalDateTime = LocalDateTime.now(),
+) : Comparable<Book> {
+    fun deepCopy(
+        title: String = this.title,
+        author: String = this.author,
+        publishedDate: LocalDateTime = this.publishedDate,
+    ) = Book(
+        title,
+        author,
+        LocalDateTime.of(
+            publishedDate.year,
+            publishedDate.month,
+            publishedDate.dayOfMonth,
+            publishedDate.hour,
+            publishedDate.minute,
+            publishedDate.second,
+        ),
+    )
+
+    override fun compareTo(other: Book): Int {
+        return other.publishedDate.compareTo(this.publishedDate)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Book
+
+        if (title != other.title) return false
+        if (publishedDate.year != other.publishedDate.year) return false
+        if (publishedDate.month != other.publishedDate.month) return false
+        if (publishedDate.dayOfMonth != other.publishedDate.dayOfMonth) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + publishedDate.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Book(title='$title', author='$author', publishedDate=$publishedDate)"
+    }
+}
