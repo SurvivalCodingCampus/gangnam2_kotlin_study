c2983ac3e9e00bfd6b4f047a2afd04a59786a37a
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-12 15:53:14 +0900

    feat: 제네릭 연습문제 2

--- src/main/kotlin/com/survival/kotlinstudy/08_generic/StrongBox.kt
+++ src/main/kotlin/com/survival/kotlinstudy/08_generic/StrongBox.kt
@@ -1,10 +1,26 @@
 package com.survival.kotlinstudy.`08_generic`
 
-class StrongBox<E> {
-    private var _data : E? = null
+enum class KeyType {
+    PADLOCK, BUTTON, DIAL, FINGER
+}
+
+class StrongBox<E>(keyType: KeyType) {
+    private var _data: E? = null
+
+    private var lockCount = when (keyType) {
+        KeyType.PADLOCK -> 1024
+        KeyType.BUTTON -> 10000
+        KeyType.DIAL -> 30000
+        KeyType.FINGER -> 1000000
+    }
+
     fun put(data: E) {
         _data = data
     }
 
-    fun get(): E? = _data
+    fun get(): E? {
+        if (lockCount == 0) return _data
+        lockCount -= 1
+        return null
+    }
 }
