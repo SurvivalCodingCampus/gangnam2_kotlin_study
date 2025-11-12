065225c945730fa7bdc5354a2531e88e56169a42
Neouul <fluctu0q0@gmail.com>
2025-11-12 15:38:18 +0900

    Feat: 문자열 조작 연습문제 1, 2

---
+++ src/main/kotlin/com/neouul/sesac/08-string/Word.kt
@@ -0,0 +1,18 @@
+package com.neouul.sesac.`08-string`
+
+class Word(var word: String) {
+
+    fun isVowel(index: Int): Boolean {
+        // char형이라서 string 활용하는 것으로 수정
+//        return when(word[index]){
+//            'a', 'e', 'i', 'o', 'u' -> true
+//            else -> false
+//        }
+        val target = word.substring(index, index+1)
+        return target == "a" || target == "e" || target == "i" || target == "u" || target == "o"
+    }
+
+    fun isConsonant(index: Int): Boolean {
+        return !isVowel(index)
+    }
+}
