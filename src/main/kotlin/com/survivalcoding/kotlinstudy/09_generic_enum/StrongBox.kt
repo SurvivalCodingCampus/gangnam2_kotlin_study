5337b0109da5fdd8e4e02dec341350be78e528ec
Jieun Lee <geniee1750@gmail.com>
2025-11-12 16:27:09 +0900

    [과제] feat: 연습문제 2 풀이 제출

--- src/main/kotlin/com/survivalcoding/kotlinstudy/09_generic_enum/StrongBox.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/09_generic_enum/StrongBox.kt
@@ -2,7 +2,9 @@ package com.survivalcoding.kotlinstudy.`09_generic_enum`
 
 // 연습문제 1. 금고 클래스 작성
 // 금고에 담는 인스턴스의 타입은 미정(generic)
-class StrongBox<E> {
+class StrongBox<E>(
+    private val keyType: KeyType   // 열쇠 종류 (enum)
+) {
     // 1개의 인스턴스만 담을 수 있음
     // 언더스코어는 내부에서만 쓰이는 변수임을 나타내기 위함 (그냥 data 라고 썼어도 무방)
     private var _data: E? = null
@@ -12,6 +14,29 @@ class StrongBox<E> {
         _data = data
     }
 
+    private var count: Int = 0  // 시도 횟수
+
     // 인스턴스 얻음, 별도의 타입 캐스팅 X
-    fun get(): E? = _data
+    fun get(): E? {
+        // 연습문제 2. get 메서드 호출
+        count++  // 호출할 때마다 시도 횟수 증가
+
+        if (count < keyType.limit) {
+            return null // 한도 도달 전 null 리턴
+        } else {
+            count = 0   // 시도 횟수 리셋
+            return _data    // 한도 도달시 잠금 해제
+        }
+    }
+}
+
+
+// 연습문제 2. 열거형 정의와 시도 횟수 한계
+enum class KeyType(
+    val limit: Int
+) {
+    PADLOCK(1024),
+    BUTTON(10000),
+    DIAL(30000),
+    FINGER(1000000)
 }
