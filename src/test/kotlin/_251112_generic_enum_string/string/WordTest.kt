100148ffa7badd5ae3f446d2a7a05b06332c038d
MisterJerry <misterjerry12345@gmail.com>
2025-11-12 17:48:07 +0900

    feat: StringIndexOutOfBoundsException 관련 test코드 추가 작성

--- src/test/kotlin/_251112_generic_enum_string/string/WordTest.kt
+++ src/test/kotlin/_251112_generic_enum_string/string/WordTest.kt
@@ -8,16 +8,31 @@ class WordTest {
     fun `isVowel함수가 잘 동작하는지 테스트`() {
         //given
         val word = Word("a")
+        //when
+        val result = word.isVowel(0)
         //then
-        assertEquals(true, word.isVowel(0))
+        assertEquals(true, result)
     }
 
     @Test
     fun `isConsonant함수가 잘 동작하는지 테스트`() {
         //given
         val word = Word("a")
+        //when
+        val result = word.isConsonant(0)
         //then
-        assertEquals(false, word.isConsonant(0))
+        assertEquals(false, result)
+    }
+
+    @Test(expected = StringIndexOutOfBoundsException::class)
+    fun `글자의 길이를 넘어서는 index를 접근할 경우 터지는지 테스트`() {
+        //given
+        val word = Word("a")
+        //when
+        word.isVowel(word.word.length + 1)
+        //then
+        //터짐
+
     }
 
 }
