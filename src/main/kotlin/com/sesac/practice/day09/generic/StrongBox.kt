c9cdefab92bfd75bb4938b52e8690e4e2deb8f19
japygo <hyunnjinn@gmail.com>
2025-11-12 16:19:26 +0900

    feat: 제네릭, 열거형 연습문제 2

--- src/main/kotlin/com/sesac/practice/day09/generic/StrongBox.kt
+++ src/main/kotlin/com/sesac/practice/day09/generic/StrongBox.kt
@@ -1,11 +1,25 @@
 package com.sesac.practice.day09.generic
 
 class StrongBox<T>(
+    private val keyType: KeyType,
     private var item: T? = null,
+    private var tryCount: Int = INIT_TRY_COUNT,
 ) {
     fun put(item: T) {
         this.item = item
     }
 
-    fun get(): T? = item
+    fun get(): T? {
+        tryCount++
+
+        if (keyType.canOpen(tryCount)) {
+            return item
+        }
+
+        return null
+    }
+
+    companion object {
+        const val INIT_TRY_COUNT = 0
+    }
 }
--- src/test/kotlin/com/sesac/practice/day09/generic/StrongBoxTest.kt
+++ src/test/kotlin/com/sesac/practice/day09/generic/StrongBoxTest.kt
@@ -2,16 +2,19 @@ package com.sesac.practice.day09.generic
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class StrongBoxTest {
     @Test
     fun `금고를 생성한다`() {
         // given
+        val keyType = KeyType.PADLOCK
         val item = 1
+        val tryCount = keyType.tryLimit
 
         // when
-        val strongBox: StrongBox<Int> = StrongBox(item)
+        val strongBox: StrongBox<Int> = StrongBox(keyType, item, tryCount)
 
         // then
         assertEquals(item, strongBox.get())
@@ -21,14 +24,47 @@ class StrongBoxTest {
     @Test
     fun `금고에 아이템을 넣는다`() {
         // given
-        val strongBox = StrongBox<String>()
+        val keyType = KeyType.PADLOCK
         val item = "item"
+        val tryCount = keyType.tryLimit
+        val strongBox: StrongBox<String> = StrongBox(keyType, item, tryCount)
 
         // when
-        strongBox.put(item)
+        val changeItem = "change item"
+        strongBox.put(changeItem)
 
         // then
-        assertEquals(item, strongBox.get())
+        assertEquals(changeItem, strongBox.get())
         assertTrue(strongBox.get() is String)
+    }
+
+    @Test
+    fun `금고에서 열쇠의 사용횟수에 도달하면 잠금이 해제되어 열린다`() {
+        // given
+        val keyType = KeyType.PADLOCK
+        val item = "item"
+        val tryCount = keyType.tryLimit - 1
+        val strongBox: StrongBox<String> = StrongBox(keyType, item, tryCount)
+
+        // when
+        val get = strongBox.get()
+
+        // then
+        assertEquals(item, get)
+    }
+
+    @Test
+    fun `금고에서 열쇠의 사용횟수 도달하기 전에는 null을 리턴한다`() {
+        // given
+        val keyType = KeyType.PADLOCK
+        val item = "item"
+        val tryCount = keyType.tryLimit - 2
+        val strongBox = StrongBox(keyType, item, tryCount)
+
+        // when
+        val get = strongBox.get()
+
+        // then
+        assertNull(get)
     }
 }
