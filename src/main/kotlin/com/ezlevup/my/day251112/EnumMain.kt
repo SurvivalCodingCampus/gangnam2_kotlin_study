d5b1eaa5f043445f2049e0ed286fb97509f28a32
이영현 <hong.git@yahoo.com>
2025-11-12 16:08:18 +0900

    feat: 제네릭, 열거형 정리

---
+++ src/main/kotlin/com/ezlevup/my/day251112/EnumMain.kt
@@ -0,0 +1,19 @@
+package com.ezlevup.my.day251112
+
+enum class AuthState {
+    AUTH,
+    UN,
+    UNKNOWN,
+}
+
+fun something(authState: AuthState) {
+    when (authState) {
+        AuthState.AUTH -> TODO()
+        AuthState.UN -> TODO()
+        AuthState.UNKNOWN -> TODO()
+    }
+}
+
+fun main() {
+
+}
