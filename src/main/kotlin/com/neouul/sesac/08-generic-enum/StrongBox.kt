5eed568190a927f61c3ef95d9b9adfc131b772e6
Neouul <fluctu0q0@gmail.com>
2025-11-12 16:17:09 +0900

    Feat: 제네릭, 열거형 연습문제 2

--- src/main/kotlin/com/neouul/sesac/08-generic-enum/StrongBox.kt
+++ src/main/kotlin/com/neouul/sesac/08-generic-enum/StrongBox.kt
@@ -1,11 +1,26 @@
 package com.neouul.sesac.`08-generic-enum`
 
-class StrongBox<T> {
+class StrongBox<T>(private var keyType: KeyType) {
     private var _data: T? = null
+    private var keyCounter = 0
 
     fun put(data: T) {
         _data = data
     }
 
-    fun get(): T? = _data
+    fun get(): T? {
+        if(keyCounter == keyType.keyNumber) return _data
+        keyCounter++
+        return null
+    }
+}
+
+// Since each enum is an instance of the enum class, it can be initialized
+// 각 enum은 KeyType라는 이름의 enum class의 인스턴스임
+// 즉, 각 enum은 생성자를 통해 필드를 초기화할 수 있음
+enum class KeyType(val keyNumber: Int) {
+    PADLOCK(1024),
+    BUTTON(10000),
+    DIAL(30000),
+    FINGER(1000000),
 }
