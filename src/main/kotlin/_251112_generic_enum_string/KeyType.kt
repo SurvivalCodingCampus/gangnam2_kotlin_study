637a4338743dd180e4dba5b023cf4eaeac0f3eb2
MisterJerry <misterjerry12345@gmail.com>
2025-11-12 17:14:50 +0900

    fix: 매직넘버 고려하여 enum 클래스 재설계

--- src/main/kotlin/_251112_generic_enum_string/KeyType.kt
+++ src/main/kotlin/_251112_generic_enum_string/KeyType.kt
@@ -1,5 +1,9 @@
 package _251112_generic_enum_string
 
-enum class KeyType {
-    PADLOCK, BUTTON, DIAL, FINGER
+enum class KeyType(val cnt: Int) {
+    //열쇠의 종류별로 시도 횟수 저장하기 위한 생성자
+    PADLOCK(1024),
+    BUTTON(10000),
+    DIAL(30000),
+    FINGER(1000000)
 }
