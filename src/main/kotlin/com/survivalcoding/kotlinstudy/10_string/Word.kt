60629a6adb99bfff40e3b893f4053ae4a8a90365
Jieun Lee <geniee1750@gmail.com>
2025-11-12 16:57:09 +0900

    [과제] feat: 문자열 연습문제 2 풀이 제출

--- src/main/kotlin/com/survivalcoding/kotlinstudy/10_string/Word.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/10_string/Word.kt
@@ -1,11 +1,24 @@
 package com.survivalcoding.kotlinstudy.`10_string`
 
-// 연습문제 1. isVowel 함수 완성
 class Word(var word: String) {
+    private val vowelList = listOf("a", "e", "i", "o", "u")
 
+    // 연습문제 1. isVowel 함수 완성
     fun isVowel(i: Int): Boolean {
         val char = word.substring(i, i + 1).lowercase()
-        val vowelList = listOf("a", "e", "i", "o", "u")
         return char in vowelList
     }
+
+    // 연습문제 2. isConsonant 함수 완성
+    fun isConsonant(i: Int): Boolean {
+        val char = word.substring(i, i + 1).lowercase()
+        return char !in vowelList
+    }
+}
+
+
+fun main() {
+    val w = Word("APPLE")
+    println(w.isVowel(0))   // true
+    println(w.isConsonant(0))   // false
 }
