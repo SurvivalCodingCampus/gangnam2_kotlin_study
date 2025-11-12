32425cff0030529af0a4b43f6313b2b01fee7a3d
Jieun Lee <geniee1750@gmail.com>
2025-11-12 19:27:39 +0900

    [과제] fix: StrongBox 클래스 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/09_generic_enum/StrongBox.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/09_generic_enum/StrongBox.kt
@@ -3,7 +3,7 @@ package com.survivalcoding.kotlinstudy.`09_generic_enum`
 // 연습문제 1. 금고 클래스 작성
 // 금고에 담는 인스턴스의 타입은 미정(generic)
 class StrongBox<E>(
-    private val keyType: KeyType   // 열쇠 종류 (enum)
+    val keyType: KeyType   // 열쇠 종류 (enum)
 ) {
     // 1개의 인스턴스만 담을 수 있음
     // 언더스코어는 내부에서만 쓰이는 변수임을 나타내기 위함 (그냥 data 라고 썼어도 무방)
@@ -14,7 +14,7 @@ class StrongBox<E>(
         _data = data
     }
 
-    private var count: Int = 0  // 시도 횟수
+    var count: Int = 0
 
     // 인스턴스 얻음, 별도의 타입 캐스팅 X
     fun get(): E? {
