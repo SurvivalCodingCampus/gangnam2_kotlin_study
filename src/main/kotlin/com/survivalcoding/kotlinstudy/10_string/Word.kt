cc4356b6a5822730e4e60ce5f58cac33253e3cef
Jieun Lee <geniee1750@gmail.com>
2025-11-12 19:10:45 +0900

    [과제] fix: Word 클래스 예외 반영 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/10_string/Word.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/10_string/Word.kt
@@ -5,14 +5,16 @@ class Word(var word: String) {
 
     // 연습문제 1. isVowel 함수 완성
     fun isVowel(i: Int): Boolean {
+        if (i !in word.indices) return false    // 인덱스를 벗어난 i 가 들어오면 false
         val char = word.substring(i, i + 1).lowercase()
-        return char in vowelList
+        return char in "a".."z" && char in vowelList    // 알파벳인지 확인
     }
 
     // 연습문제 2. isConsonant 함수 완성
     fun isConsonant(i: Int): Boolean {
+        if (i !in word.indices) return false
         val char = word.substring(i, i + 1).lowercase()
-        return char !in vowelList
+        return char in "a".."z" && char !in vowelList   // 알파벳인지 확인
     }
 }
 
