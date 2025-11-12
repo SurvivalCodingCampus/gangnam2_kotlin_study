84bf6dd344ff1345fc6185c28187a63fc3791d67
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-12 16:34:11 +0900

    feat: 문자열 연습문제 2

--- src/main/kotlin/com/survival/kotlinstudy/09_string/Word.kt
+++ src/main/kotlin/com/survival/kotlinstudy/09_string/Word.kt
@@ -1,8 +1,13 @@
 package com.survival.kotlinstudy.`09_string`
 
 class Word(var word: String) {
+    private val vowelList = listOf("a", "e", "i", "o", "u")
 
     fun isVowel(i: Int): Boolean {
-        return listOf("a","e","i","o","u").contains(word.substring(i, i + 1))
+        return vowelList.contains(word.substring(i, i + 1).lowercase())
+    }
+
+    fun isConsonant(i: Int): Boolean {
+        return !vowelList.contains(word.substring(i, i + 1).lowercase())
     }
 }
