00943d90573774e5b9dd2e016296fadd28f57f7c
Neouul <fluctu0q0@gmail.com>
2025-11-12 17:36:52 +0900

    Feat: Word 메서드 대소문자 구분, 문장 부호 여부 처리

--- src/main/kotlin/com/neouul/sesac/08-string/Word.kt
+++ src/main/kotlin/com/neouul/sesac/08-string/Word.kt
@@ -8,11 +8,19 @@ class Word(var word: String) {
 //            'a', 'e', 'i', 'o', 'u' -> true
 //            else -> false
 //        }
-        val target = word.substring(index, index + 1)
+
+        // 대소문자 구분 없애기
+        val lowerCaseWord = word.lowercase()
+        val target = lowerCaseWord.substring(index, index + 1)
         return target == "a" || target == "e" || target == "i" || target == "u" || target == "o"
     }
 
     fun isConsonant(index: Int): Boolean {
-        return !isVowel(index)
+        // 모음이라면 false 리턴
+        if (isVowel(index)) return false
+
+        // 문자라면 true, 아니라면(문장부호 등) false 리턴
+        val target = word.substring(index, index + 1)
+        return target.single().isLetter()   // Returns true if this character is a letter
     }
 }
