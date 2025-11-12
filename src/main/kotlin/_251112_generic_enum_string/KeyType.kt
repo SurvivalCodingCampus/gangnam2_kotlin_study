100668168c8ca29880d2c7792e817863f8e04805
MisterJerry <misterjerry12345@gmail.com>
2025-11-12 16:44:17 +0900

    feat: 연습문제2, 요구사항에 맞게 StrongBox클래스 수정 및 Enum클래스 설계

---
+++ src/main/kotlin/_251112_generic_enum_string/KeyType.kt
@@ -0,0 +1,5 @@
+package _251112_generic_enum_string
+
+enum class KeyType {
+    PADLOCK, BUTTON, DIAL, FINGER
+}
--- src/main/kotlin/_251112_generic_enum_string/StrongBox.kt
+++ src/main/kotlin/_251112_generic_enum_string/StrongBox.kt
@@ -1,13 +1,50 @@
 package _251112_generic_enum_string
 
-class StrongBox<T> {
+class StrongBox<T>(keyType: KeyType) {
     private var instance: T? = null
+    private var cnt = 0 //외부에서 횟수 조작 못하게 private로 막음
+    val kt = keyType
+
+    init {
+        when (kt) {
+            KeyType.PADLOCK -> {
+                cnt = 1024
+            }
+
+            KeyType.BUTTON -> {
+                cnt = 10000
+            }
+
+            KeyType.DIAL -> {
+                cnt = 30000
+            }
+
+            KeyType.FINGER -> {
+                cnt = 1000000
+            }
+        }
+
+    }
 
     fun put(newInstance: T) {
         instance = newInstance
     }
 
     fun get(): T? {
-        return instance
+        if (cnt == 0) {
+            return instance
+        } else {
+            cnt -= 1
+            println("count가 ${cnt}번 남아 금고를 열 수 없습니다.")
+            return null
+        }
+
+    }
+}
+
+fun main() {
+    val strongBox = StrongBox<String>(KeyType.PADLOCK)
+    repeat(1025) {
+        strongBox.get()
     }
 }
