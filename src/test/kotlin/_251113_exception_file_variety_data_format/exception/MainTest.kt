783761e3cd9dd5ffe64cc5f28544e7033352e90e
KunHee Jung <misterjerry@sju.ac.kr>
2025-11-13 23:43:52 +0900

    feat: Exception관련 테스트코드 추가

---
+++ src/test/kotlin/_251113_exception_file_variety_data_format/exception/MainTest.kt
@@ -0,0 +1,13 @@
+package _251113_exception_file_variety_data_format.exception
+
+import org.junit.Test
+
+
+class MainTest {
+    @Test
+    fun `프로그램이 터지지 않고 잘 돌아가는지 테스트`(){
+        //given
+        main()
+    }
+
+}
