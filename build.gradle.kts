ca337d8de8814048e591957ce145d47506c858e5
Neouul <fluctu0q0@gmail.com>
2025-11-13 16:50:14 +0900

    Chore: 직렬화 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 group = "com.survivalcoding"
@@ -12,6 +13,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 }
 
 tasks.test {
