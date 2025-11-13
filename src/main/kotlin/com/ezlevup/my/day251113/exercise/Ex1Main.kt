aa6ac72252e39186dac4744ea846ad0d559576db
이영현 <hong.git@yahoo.com>
2025-11-13 16:25:31 +0900

    feat: 연습문제

---
+++ src/main/kotlin/com/ezlevup/my/day251113/exercise/Ex1Main.kt
@@ -0,0 +1,39 @@
+package com.ezlevup.my.day251113.exercise
+
+class Ex1 {
+    companion object {
+        fun toIntOrZero(s: String): Int {
+            try {
+                return s.toInt()
+            } catch (e: NumberFormatException) {
+                0
+            } catch (e: Exception) {
+                0
+            }
+            return 0
+        }
+    }
+}
+
+
+// val String.toIntOrZero: Int
+//    get() = toIntOrNull() ?: 0
+
+public fun String.toIntOrZero(): Int = toIntOrNull() ?: 0
+
+fun main() {
+    val numString: String = "10.8"
+
+    val num: Int = Ex1.toIntOrZero(numString)
+    println(num)
+
+    // val num = numString.toIntOrNull()
+    // println(num)
+
+    // val num = numString.toIntOrZero
+    // println(num)
+
+    // val num = numString.toIntOrZero()
+    // println(num)
+
+}
