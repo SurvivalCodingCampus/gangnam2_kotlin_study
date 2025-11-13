6df738d4ed8036a47a3e2804dbbbc8f54d54b026
Jieun Lee <geniee1750@gmail.com>
2025-11-13 17:30:32 +0900

    [과제] fix: 연습문제 1 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/11_exception_file_data/Exception.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/11_exception_file_data/Exception.kt
@@ -3,18 +3,23 @@ package com.survivalcoding.kotlinstudy.`11_exception_file_data`
 
 fun main() {
     val numString = "10.5"
+    val num = StringToInt(numString)
+    println(num)
+}
 
-    val num: Int = try {
-        numString.toInt()
-    } catch (e: NumberFormatException) {
-        println("NumberFormatException : ${e.message} ")
+fun StringToInt(str: String): Int {
+    return try {
+        str.toInt() // 숫자로 변환
+    }   // 예외 처리
+    catch (e: NumberFormatException) {
+        println("숫자형 형태 변환 오류: ${e.message}")
+        0
+    } catch (e: IllegalArgumentException) {
+        println("잘못된 인자 오류: ${e.message}")
         0
     } catch (e: Exception) {
-        e.printStackTrace()
+        println("예상치 못한 오류: ${e.message}")
         0
     }
-
-    println(num)
-
 }
 
