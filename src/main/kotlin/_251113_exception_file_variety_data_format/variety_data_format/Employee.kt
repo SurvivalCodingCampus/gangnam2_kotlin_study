126e7eaa59fba647c84959628c6fb613844df371
MisterJerry <misterjerry12345@gmail.com>
2025-11-13 19:25:20 +0900

    feat: 연습문제, 직렬화되게 클래스 수정

---
+++ src/main/kotlin/_251113_exception_file_variety_data_format/variety_data_format/Employee.kt
@@ -0,0 +1,9 @@
+﻿package _251113_exception_file_variety_data_format.variety_data_format
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Employee(
+    var name: String,
+    var age: Int
+)
