00af085fbe01e65a1a2cc966a40624c2e61f6f59
Jieun Lee <geniee1750@gmail.com>
2025-11-14 17:15:26 +0900

    [과제] feat: 연습문제 1 풀이 제출

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/12_debugging_lamda/ChartData.kt
@@ -0,0 +1,37 @@
+package com.survivalcoding.kotlinstudy.`12_debugging_lamda`
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+data class CollectionChartDataList(
+    val collectionChartDataList: List<CollectionChartData>
+)
+
+@Serializable
+data class CollectionChartData(
+    val collectionName: String,
+    val collectionSalePrice: List<CollectionSalePrice> = emptyList()
+)
+
+@Serializable
+data class CollectionSalePrice(
+    val price: Double,
+    val cvtDatetime: String
+)
+
+// 역직렬화
+fun deserialize(source: File): CollectionChartDataList {
+    val jsonText = source.readText()
+    return Json.decodeFromString(jsonText)
+
+}
+
+fun main() {
+    val file = File("src/main/kotlin/com/survivalcoding/kotlinstudy/12_debugging_lamda/chart_data.json")
+
+    val data = deserialize(file)
+
+    println(data)
+}
