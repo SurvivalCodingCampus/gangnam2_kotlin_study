f27c4082b7e578b834853a8508278ba8e8300d0c
japygo <hyunnjinn@gmail.com>
2025-11-14 15:48:55 +0900

    feat: 디버깅 연습문제 1

---
+++ src/test/kotlin/com/sesac/practice/day11/debug/ChartDataTest.kt
@@ -0,0 +1,26 @@
+package com.sesac.practice.day11.debug
+
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class ChartDataTest {
+    @Test
+    fun `json 차트 데이터를 역직렬화한다`() {
+        // given
+        val file = File("chart_data.json")
+        val json = file.readText()
+
+        // when
+        val chartData = Json.decodeFromString<ChartData>(json)
+
+        // then
+        assertEquals(75, chartData.collectionChartDataList?.size)
+
+        val diffData = chartData.collectionChartDataList?.get(67)
+        assertEquals("collection68", diffData?.collectionName)
+        assertNull(diffData?.collectionSalePrice)
+    }
+}
