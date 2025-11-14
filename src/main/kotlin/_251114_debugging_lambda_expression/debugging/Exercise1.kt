b234c35130af2e7b2484c24e332318771694aeaa
MisterJerry <misterjerry12345@gmail.com>
2025-11-14 18:42:27 +0900

    feat: 연습문제1 풀이

---
+++ src/main/kotlin/_251114_debugging_lambda_expression/debugging/Exercise1.kt
@@ -0,0 +1,29 @@
+﻿package _251114_debugging_lambda_expression.debugging
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+data class CollectionChartDataList(
+    val collectionChartDataList: List<CollectionChartData>
+)
+
+@Serializable
+data class CollectionChartData(
+    val collectionName: String,
+    val collectionSalePrice: List<CollectionSale>,
+)
+
+@Serializable
+data class CollectionSale(
+    val price: Double,
+    val cvtDatetime: String
+)
+
+fun main() {
+    val file = File("chart_data.txt").readText()
+    val collectionChartDataList: CollectionChartDataList = Json.decodeFromString(file)
+    println(collectionChartDataList)
+
+}
