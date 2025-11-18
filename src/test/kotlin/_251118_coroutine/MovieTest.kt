61a29334b37fa812b2562c8c4fe2f3d3eaf8d053
MisterJerry <misterjerry12345@gmail.com>
2025-11-18 19:04:12 +0900

    feat: 연습문제1 풀이 및 테스트코드 작성

---
+++ src/test/kotlin/_251118_coroutine/MovieTest.kt
@@ -0,0 +1,16 @@
+﻿package _251118_coroutine
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MovieTest {
+    @Test
+    fun `역직렬화가 잘 되었는지 테스트`() = runTest {
+        //when
+        val deserialization = getMovieInfo()
+        //then
+        assertEquals("Star Wars", deserialization.title)
+    }
+
+}
