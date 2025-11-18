37905844ccd74719c7ee954a24708c83e45f1273
whoamixzerone <whoamixzerone@gmail.com>
2025-11-18 17:03:30 +0900

    feat: 연습문제 1 완료

---
+++ src/test/kotlin/com/survivaalcoding/kotlinstudy/12_async/example/MovieTest.kt
@@ -0,0 +1,45 @@
+package com.survivaalcoding.kotlinstudy.`12_async`.example
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MovieTest {
+    @Test
+    fun `Movie 인스턴스를 생성할 수 있다`() {
+        // given
+        val title = "Star Wars"
+        val director = "George Lucas"
+        val year = 1977
+
+        // when
+        val movie = Movie(title, director, year)
+
+        // then
+        assertThat(movie.title).isEqualTo(title)
+        assertThat(movie.director).isEqualTo(director)
+        assertThat(movie.year).isEqualTo(year)
+    }
+
+    @Test
+    fun `영화 정보를 반환하는 함수에서 역직렬화 된다`() = runBlocking {
+        // given
+        val title = "Star Wars"
+        val director = "George Lucas"
+        val year = 1977
+        val movie = Movie(title, director, year)
+
+        // when
+        val actual = MovieInfo.getMovieInfo()
+
+        // then
+        assertThat(actual.title).isEqualTo(title)
+        assertThat(actual.director).isEqualTo(director)
+        assertThat(actual.year).isEqualTo(year)
+        assertThat(actual).isEqualTo(movie)
+        assertThat(actual).isNotSameAs(movie)
+        Unit
+    }
+
+}
