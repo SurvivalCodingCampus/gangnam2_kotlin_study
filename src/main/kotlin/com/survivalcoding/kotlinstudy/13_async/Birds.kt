4b96b570177af62446f499b9227b1cad830557fa
Jieun Lee <geniee1750@gmail.com>
2025-11-18 19:14:09 +0900

    [과제] feat: 연습문제 3 풀이 제출

--- src/main/kotlin/com/survivalcoding/kotlinstudy/13_async/Birds.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/13_async/Birds.kt
@@ -1,30 +1,40 @@
 package com.survivalcoding.kotlinstudy.`13_async`
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
 // 연습문제 2. 코루틴을 사용한 새소리 타이이밍 재현
 fun main() = runBlocking {
-    val bird1 = launch {
-        repeat(4) {
-            println("꾸우")
-            delay(1000L)
+    val job = launch {
+        launch {
+            while (isActive) {
+                println("꾸우")
+                delay(1000L)
+            }
+        }
+        launch {
+            while (isActive) {
+                println("까악")
+                delay(2000L)
+            }
+        }
+        launch {
+            while (isActive) {
+                println("짹짹")
+                delay(3000L)
+            }
         }
     }
 
-    val bird2 = launch {
-        repeat(4) {
-            println("까악")
-            delay(2000L)
-        }
-    }
+    // 연습문제 3. 10초 반복
+    delay(10000L)
+    println("10초 끝")
+    job.cancel()    // 그만하라는 요청
+    job.join()  // 코루틴이 완전히 종료될 때까지 기다리는 역할
 
-    val bird3 = launch {
-        repeat(4) {
-            println("짹짹")
-            delay(3000L)
-        }
-    }
+    // 이렇게도가능
+    // job.cancelAndJoin()
 }
