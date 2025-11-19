8c33f6e0e2ce05c1822cb971825e8cb5fbace9a7
MisterJerry <misterjerry12345@gmail.com>
2025-11-19 17:27:33 +0900

    feat: 연습문제4 풀이

---
+++ src/main/kotlin/_251119_datasource/exercise4/StockDataSourceImpl.kt
@@ -0,0 +1,20 @@
+﻿package _251119_datasource.exercise4
+
+import java.io.File
+
+class StockDataSourceImpl : StockDataSource {
+    override suspend fun getStockListings(): List<StockListing> {
+        val file = File(FILE_PATH)
+        return csvToObject(file)
+    }
+}
+
+fun csvToObject(csvFile: File): List<StockListing> {
+    val resultList = mutableListOf<StockListing>()
+    csvFile.forEachLine {
+        val tmp = it.split(",")
+        resultList.add(StockListing(tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tmp[6]))
+    }
+    resultList.removeAt(0) //첫줄은 제목이므로 제거
+    return resultList
+}
---
+++ src/main/kotlin/_251119_datasource/exercise4/StockListing.kt
@@ -0,0 +1,14 @@
+﻿package _251119_datasource.exercise4
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StockListing(
+    val symbol: String? = null,
+    val name: String? = null,
+    val exchange: String? = null,
+    val assetType: String? = null,
+    val ipoDate: String? = null,
+    val delistingDate: String? = null,
+    val status: String? = null
+)
