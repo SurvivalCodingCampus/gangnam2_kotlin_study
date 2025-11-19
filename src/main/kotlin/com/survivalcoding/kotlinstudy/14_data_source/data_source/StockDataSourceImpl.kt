d66423e9f0430a269e504e5d11622c2bff07672c
Jieun Lee <geniee1750@gmail.com>
2025-11-19 18:29:50 +0900

    [과제] fix: 연습문제 name 없는 필드 제외하도록 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/14_data_source/data_source/StockDataSourceImpl.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/14_data_source/data_source/StockDataSourceImpl.kt
@@ -13,34 +13,35 @@ class StockDataSourceImpl : StockDataSource {
 
 
         // 한 줄씩 읽으면서 mapping
-        return rawData.map { line ->
-            val stocks = line.split(",")
+        return rawData
+            .map { it.split(",") }
+            .filter { stocks -> stocks[1].isNotBlank() }
+            .map { stocks ->
+                val symbol = stocks[0]
+                val name = stocks[1]
+                val exchange = stocks[2]
+                val assetType = stocks[3]
 
-            val symbol = stocks[0]
-            val name = stocks[1]
-            val exchange = stocks[2]
-            val assetType = stocks[3]
+                val ipoDate = LocalDate.parse(stocks[4])
 
-            val ipoDate = LocalDate.parse(stocks[4])
+                val delistingDate =
+                    if (stocks[5] == "null") {
+                        null
+                    } else {
+                        LocalDate.parse(stocks[5])
+                    }
 
-            val delistingDate =
-                if (stocks[5] == "null") {
-                    null
-                } else {
-                    LocalDate.parse(stocks[5])
-                }
+                val status = stocks[6]
 
-            val status = stocks[6]
-
-            StockListing(
-                symbol = symbol,
-                name = name,
-                exchange = exchange,
-                assetType = assetType,
-                ipoDate = ipoDate,
-                delistingDate = delistingDate,
-                status = status
-            )
-        }
+                StockListing(
+                    symbol = symbol,
+                    name = name,
+                    exchange = exchange,
+                    assetType = assetType,
+                    ipoDate = ipoDate,
+                    delistingDate = delistingDate,
+                    status = status
+                )
+            }
     }
 }
