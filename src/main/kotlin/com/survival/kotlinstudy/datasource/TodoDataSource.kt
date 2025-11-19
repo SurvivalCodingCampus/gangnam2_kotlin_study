9cb55bec8b22ddd22295a9fe9fa2e539a191dec3
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-19 14:23:07 +0900

    feat: datasource 연습문제 2

--- src/main/kotlin/com/survival/kotlinstudy/datasource/TodoDataSource.kt
+++ src/main/kotlin/com/survival/kotlinstudy/datasource/TodoDataSource.kt
@@ -2,4 +2,6 @@ package com.survival.kotlinstudy.datasource
 
 interface TodoDataSource {
     suspend fun getTodo(): Todo
+
+    suspend fun getTodos(): List<Todo>
 }
--- src/main/kotlin/com/survival/kotlinstudy/datasource/TodoDataSourceImpl.kt
+++ src/main/kotlin/com/survival/kotlinstudy/datasource/TodoDataSourceImpl.kt
@@ -1,7 +1,5 @@
 package com.survival.kotlinstudy.datasource
 
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import java.io.File
 
@@ -9,5 +7,10 @@ class TodoDataSourceImpl : TodoDataSource {
     override suspend fun getTodo(): Todo {
         val file = File("todo.json")
         return Json.decodeFromString<Todo>(file.readText())
+    }
+
+    override suspend fun getTodos(): List<Todo> {
+        val file = File("todos.json")
+        return Json.decodeFromString(file.readText())
     }
 }
