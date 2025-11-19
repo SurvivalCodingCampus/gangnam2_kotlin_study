33f88cc63aff7298d65ab62352bce50795cae310
이영현 <hong.git@yahoo.com>
2025-11-19 17:36:08 +0900

    feat: DataSource 연습문제 4

---
+++ src/main/kotlin/com/ezlevup/my/day251119/exercise/Ex4DataSource.kt
@@ -0,0 +1,48 @@
+package com.ezlevup.my.day251119.exercise
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import java.io.File
+
+
+@Serializable
+data class StockListing(
+    var symbol: String? = null,
+    var name: String? = null,
+    var exchange: String? = null,
+    var assetType: String? = null,
+    var ipoDate: String? = null,
+    var delistingDate: String? = null,
+    var status: String? = null,
+)
+
+interface StockDataSource {
+    suspend fun getStockListings(): List<StockListing>
+}
+
+class StockDataSourceImpl : StockDataSource {
+    override suspend fun getStockListings(): List<StockListing> {
+        val file = File("listing_status.csv")
+        val stockLines = file.readLines()
+
+        val stocks = mutableListOf<StockListing>()
+
+        for (i in stockLines.indices) {
+            if (i == 0) continue
+            val line = stockLines[i]
+            val row = line.split(',')
+            val stock = StockListing(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+            stocks.add(stock)
+        }
+
+        return stocks
+    }
+}
+
+fun main(): Unit = runBlocking {
+    val stockDataSource = StockDataSourceImpl()
+    val stockData = stockDataSource.getStockListings()
+    stockData.take(5).forEach { println(it) }
+    println(stockData.count())
+}
+
