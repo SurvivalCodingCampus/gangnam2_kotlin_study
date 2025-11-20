108be344e845cec85775dead5cbd5a5d8c28baad
MisterJerry <misterjerry12345@gmail.com>
2025-11-20 18:01:06 +0900

    feat: 연습문제1 모의객체 설계 및 테스트코드 작성

---
+++ src/test/kotlin/_251120_test_double_model_class_repository/exercise1/FileCommentRepositoryImplTest.kt
@@ -0,0 +1,21 @@
+﻿package _251120_test_double_model_class_repository.exercise1
+
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FileCommentRepositoryImplTest {
+    val mockComment = MockCommentDatasourceImpl()
+    val repository = FileCommentRepositoryImpl(mockComment)
+
+    @Test
+    fun `postId가 1인 모의 데이터의 수는 1개이다`() = runTest {
+        assertEquals(1, repository.getComments(1).size)
+    }
+
+    @Test
+    fun `postId가 9인 모의 데이터의 수는 없다`() = runTest {
+        assertEquals(0, repository.getComments(9).size)
+    }
+
+}
