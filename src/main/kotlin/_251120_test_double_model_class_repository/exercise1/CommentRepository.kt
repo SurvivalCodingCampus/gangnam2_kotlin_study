b922d5f942f54d7dc59efc1e32e8834599eecce1
MisterJerry <misterjerry12345@gmail.com>
2025-11-20 20:04:31 +0900

    rename: 파일 이름 헷갈리지 않게 "File" 접두사 제거

--- src/main/kotlin/_251120_test_double_model_class_repository/exercise1/CommentRepository.kt
+++ src/main/kotlin/_251120_test_double_model_class_repository/exercise1/CommentRepository.kt
@@ -1,5 +1,5 @@
 ﻿package _251120_test_double_model_class_repository.exercise1
 
-interface FileCommentRepository {
+interface CommentRepository {
     suspend fun getComments(postId: Int): List<Comment> //데이터 원형을 가공해서 뿌려준다.
 }
--- src/main/kotlin/_251120_test_double_model_class_repository/exercise1/CommentRepositoryImpl.kt
+++ src/main/kotlin/_251120_test_double_model_class_repository/exercise1/CommentRepositoryImpl.kt
@@ -4,16 +4,16 @@ import _251120_test_double_model_class_repository.common_config.FILEPATH1
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class FileCommentRepositoryImpl(
+class CommentRepositoryImpl(
     private val commentDataSource: CommentDataSource
-) : FileCommentRepository {
+) : CommentRepository {
     override suspend fun getComments(postId: Int): List<Comment> {
         return commentDataSource.getComments().filter { it.postId == postId }
     }
 }
 
 fun main(): Unit = runBlocking {
-    val commentRepositoryImpl = FileCommentRepositoryImpl(CommentDataSourceImpl(FILEPATH1))
+    val commentRepositoryImpl = CommentRepositoryImpl(CommentDataSourceImpl(FILEPATH1))
     launch {
         println(commentRepositoryImpl.getComments(100).size)
     }
