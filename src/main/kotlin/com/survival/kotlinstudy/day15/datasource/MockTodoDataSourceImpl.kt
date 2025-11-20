3829e8758a18f4d6ded2eb1496fae7d556540edc
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 15:27:48 +0900

    feat: 연습문제 3번

---
+++ src/main/kotlin/com/survival/kotlinstudy/day15/datasource/MockTodoDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.survival.kotlinstudy.day15.datasource
+
+import com.survival.kotlinstudy.day15.model.Todo
+import kotlinx.serialization.json.Json
+import java.io.File
+
+class MockTodoDataSourceImpl(
+    private val filePath: String
+) : TodoDataSource {
+
+    override suspend fun getTodos(): List<Todo> {
+        val file = File(filePath)
+        return Json.decodeFromString(file.readText())
+    }
+}
