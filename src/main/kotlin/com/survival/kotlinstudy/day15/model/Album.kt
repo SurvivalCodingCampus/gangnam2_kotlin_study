04d1ad5dc4d91897273be1ce8632fa9443501295
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 16:09:40 +0900

    feat: 연습문제 5번 테스트

--- src/main/kotlin/com/survival/kotlinstudy/day15/model/Album.kt
+++ src/main/kotlin/com/survival/kotlinstudy/day15/model/Album.kt
@@ -1,5 +1,9 @@
 package com.survival.kotlinstudy.day15.model
 
+import kotlinx.serialization.Serializable
+
+
+@Serializable
 data class Album(
     val userId: Int,
     val id: Int,
---
+++ src/test/kotlin/com/survival/kotlinstudy/day15/repository/AlbumRepositoryImplTest.kt
@@ -0,0 +1,32 @@
+package com.survival.kotlinstudy.day15.repository
+
+import com.survival.kotlinstudy.day15.datasource.MockAlbumDataSourceImpl
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class AlbumRepositoryImplTest {
+    @Test
+    fun `AlbumRepository - getAlbums() 테스트`() = runBlocking {
+        val filePath = "data/albums.json"
+        val repository = AlbumRepositoryImpl(MockAlbumDataSourceImpl(filePath))
+        val expected = 10
+
+        val list = repository.getAlbums(expected)
+
+        assertEquals(expected,list.size)
+
+    }
+
+    @Test
+    fun `AlbumRepository - getAlbums() limit이 없을 경우 테스트`() = runBlocking {
+        val filePath = "data/albums.json"
+        val repository = AlbumRepositoryImpl(MockAlbumDataSourceImpl(filePath))
+        val expected = 100
+
+        val list = repository.getAlbums()
+
+        assertEquals(expected,list.size)
+
+    }
+}
