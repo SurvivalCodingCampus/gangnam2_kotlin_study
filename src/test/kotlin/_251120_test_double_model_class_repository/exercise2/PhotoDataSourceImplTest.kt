5516a26d87bb26099e7d434839031751598f4aeb
MisterJerry <misterjerry12345@gmail.com>
2025-11-20 20:26:46 +0900

    feat: 연습문제2 모의객체 설계 및 테스트코드 작성

---
+++ src/test/kotlin/_251120_test_double_model_class_repository/exercise2/PhotoDataSourceImplTest.kt
@@ -0,0 +1,40 @@
+﻿package _251120_test_double_model_class_repository.exercise2
+
+import _251120_test_double_model_class_repository.common_config.FILEPATH2
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PhotoDataSourceImplTest {
+    @Test
+    fun `photos_json을 읽어서 Photo객체로 변환한다`() = runTest {
+        //given
+        val photoDatasourceImpl = PhotoDataSourceImpl(FILEPATH2)
+        //when
+        val photo = photoDatasourceImpl.getAllPhotos()
+        //then
+
+        /*
+    "albumId": 100,
+    "id": 5000,
+    "title": "error quasi sunt cupiditate voluptate ea odit beatae",
+    "url": "https://via.placeholder.com/600/6dd9cb",
+    "thumbnailUrl": "https://via.placeholder.com/150/6dd9cb"
+
+         */
+
+        //맨앞
+        assertEquals(1, photo.first().albumId)
+        assertEquals(1, photo.first().id)
+        assertEquals("accusamus beatae ad facilis cum similique qui sunt", photo.first().title)
+        assertEquals("https://via.placeholder.com/600/92c952", photo.first().url)
+        assertEquals("https://via.placeholder.com/150/92c952", photo.first().thumbnailUrl)
+        //맨뒤
+        assertEquals(100, photo.last().albumId)
+        assertEquals(5000, photo.last().id)
+        assertEquals("error quasi sunt cupiditate voluptate ea odit beatae", photo.last().title)
+        assertEquals("https://via.placeholder.com/600/6dd9cb", photo.last().url)
+        assertEquals("https://via.placeholder.com/150/6dd9cb", photo.last().thumbnailUrl)
+    }
+
+}
