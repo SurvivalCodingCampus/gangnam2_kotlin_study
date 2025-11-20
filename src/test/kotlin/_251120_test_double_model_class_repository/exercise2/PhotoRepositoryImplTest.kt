5c278fc22250a2166e0598c992772f48aa6d2c55
KunHee Jung <misterjerry@sju.ac.kr>
2025-11-21 00:06:02 +0900

    feat: 연습문제2 모의객체 설계 및 테스트코드 작성

--- src/test/kotlin/_251120_test_double_model_class_repository/exercise2/PhotoRepositoryImplTest.kt
+++ src/test/kotlin/_251120_test_double_model_class_repository/exercise2/PhotoRepositoryImplTest.kt
@@ -1,5 +1,20 @@
 ﻿package _251120_test_double_model_class_repository.exercise2
 
-class PhotoRepositoryImplTest {
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
 
+class PhotoRepositoryImplTest {
+    val mockPhoto = MockPhotoDataSourceImpl()
+    val repository = PhotoRepositoryImpl(mockPhoto)
+
+    @Test
+    fun `albumId가 1인 모의 데이터의 수는 1개이다`() = runTest {
+        assertEquals(1, repository.getPhotos(1).size)
+    }
+
+    @Test
+    fun `albumId가 9인 모의 데이터의 수는 없다`() = runTest {
+        assertEquals(0, repository.getPhotos(9).size)
+    }
 }
