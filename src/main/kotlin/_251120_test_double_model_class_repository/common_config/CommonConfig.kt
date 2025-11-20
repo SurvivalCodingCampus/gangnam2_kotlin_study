a31c412f745c0a4d60abd9c3616146f16ed111f5
MisterJerry <misterjerry12345@gmail.com>
2025-11-20 16:55:22 +0900

    feat: 연습문제1 풀이

---
+++ src/main/kotlin/_251120_test_double_model_class_repository/common_config/CommonConfig.kt
@@ -0,0 +1,3 @@
+﻿package _251120_test_double_model_class_repository.common_config
+
+const val FILEPATH1 = "src\\main\\kotlin\\_251120_test_double_model_class_repository\\exercise1\\comments.json"
---
+++ src/main/kotlin/_251120_test_double_model_class_repository/exercise1/Comment.kt
@@ -0,0 +1,12 @@
+﻿package _251120_test_double_model_class_repository.exercise1
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Comment(
+    val body: String,
+    val email: String,
+    val id: Int,
+    val name: String,
+    val postId: Int
+)
