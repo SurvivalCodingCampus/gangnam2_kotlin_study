5c2ae38c04c2df3ea05d089c8255b5f0f371eaa3
Jieun Lee <geniee1750@gmail.com>
2025-11-20 19:48:18 +0900

    [과제] fix: FileDataSourceImpl 구현체 추가

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/15_data_class_repository/data_source/FileCommentDataSourceImpl.kt
@@ -0,0 +1,10 @@
+package com.survivalcoding.kotlinstudy.`15_data_class_repository`.data_source
+
+import com.survivalcoding.kotlinstudy.`15_data_class_repository`.model.Comment
+import com.survivalcoding.kotlinstudy.common.readJsonFile
+
+class FileCommentDataSourceImpl : CommentDataSource {
+    override suspend fun getComments(): List<Comment> {
+        return readJsonFile("src/main/resources/15_model_class_repository/comments.json")
+    }
+}
---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/15_data_class_repository/data_source/FileUserDataSourceImpl.kt
@@ -0,0 +1,10 @@
+package com.survivalcoding.kotlinstudy.`15_data_class_repository`.data_source
+
+import com.survivalcoding.kotlinstudy.`15_data_class_repository`.model.User
+import com.survivalcoding.kotlinstudy.common.readJsonFile
+
+class FileUserDataSourceImpl : UserDataSource {
+    override suspend fun getUsers(): List<User> {
+        return readJsonFile("src/main/resources/15_model_class_repository/users.json")
+    }
+}
