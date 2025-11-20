54f53daf6fa05aead0556599d1d3bff6c56a3dda
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 15:53:02 +0900

    feat: 연습문제 3번 테스트

---
+++ src/test/kotlin/com/survival/kotlinstudy/day15/repository/TodoRepositoryImplTest.kt
@@ -0,0 +1,41 @@
+package com.survival.kotlinstudy.day15.repository
+
+import com.survival.kotlinstudy.day15.datasource.MockTodoDataSourceImpl
+import com.survival.kotlinstudy.day15.model.Todo
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+class TodoRepositoryImplTest {
+
+    @Test
+    fun `TodoRepositoryImpl 의 getTodos() 테스트`(): Unit = runBlocking {
+        val filePath = "data/todos.json"
+        val repository = TodoRepositoryImpl(MockTodoDataSourceImpl(filePath))
+        val expected = 200
+
+
+        val list = repository.getTodos()
+
+
+        assertEquals(expected, list.size)
+        assertIs<List<Todo>>(list)
+
+
+    }
+
+    @Test
+    fun `TodoRepositoryImpl의 getCompletedTodos() 테스트`(): Unit = runBlocking {
+        val filePath = "data/todos.json"
+        val repository = TodoRepositoryImpl(MockTodoDataSourceImpl(filePath))
+
+
+        val list = repository.getCompletedTodos()
+
+
+        assertTrue { list.any { it.completed } }
+
+    }
+}
