5f1ef3baabb6aebd5d3b583cc036d762bd8d7156
KunHee Jung <misterjerry@sju.ac.kr>
2025-11-21 00:57:05 +0900

    fix: FilePath에 의존적인 것을 생성자로 옮겨서 해결

--- src/main/kotlin/_251120_test_double_model_class_repository/exercise5/AlbumDataSourceImpl.kt
+++ src/main/kotlin/_251120_test_double_model_class_repository/exercise5/AlbumDataSourceImpl.kt
@@ -4,9 +4,9 @@ import _251120_test_double_model_class_repository.common_config.FILEPATH5
 import kotlinx.serialization.json.Json
 import java.io.File
 
-class AlbumDataSourceImpl : AlbumDataSource {
+class AlbumDataSourceImpl(val filePath: String) : AlbumDataSource {
     override suspend fun getAllAlbum(): List<Album> {
-        val file = File(FILEPATH5).readText()
+        val file = File(filePath).readText()
         return Json.decodeFromString(file)
     }
 }
