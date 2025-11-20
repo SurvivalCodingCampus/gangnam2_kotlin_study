8bda04c7e21a72989ce2b599d134db6b4d0a640b
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 16:06:26 +0900

    feat: 연습문제 4번 테스트

---
+++ src/test/kotlin/com/survival/kotlinstudy/day15/repository/UserRepositoryImplTest.kt
@@ -0,0 +1,27 @@
+package com.survival.kotlinstudy.day15.repository
+
+import com.survival.kotlinstudy.day15.datasource.MockUserDataSourceImpl
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class UserRepositoryImplTest {
+
+    @Test
+    fun `UserRepository - getUsers() 메소드 테스트`() = runBlocking {
+        val filePath = "data/users.json"
+        val repository = UserRepositoryImpl(MockUserDataSourceImpl(filePath))
+        val expected = 10
+
+        assertEquals(expected, repository.getUsers().size)
+    }
+
+    @Test
+    fun `UserRepository - getUsersTop10ByUserName() 메소드 테스트`() = runBlocking {
+        val filePath = "data/users.json"
+        val repository = UserRepositoryImpl(MockUserDataSourceImpl(filePath))
+        val expected = 10
+
+        assertEquals(expected, repository.getUsersTop10ByUserName().size)
+    }
+}
