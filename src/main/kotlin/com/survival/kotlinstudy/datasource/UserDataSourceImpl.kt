63295a6b043f6d587786958a3be618058e218e45
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 15:28:30 +0900

    feat: users.json 이름 변경

--- src/main/kotlin/com/survival/kotlinstudy/datasource/UserDataSourceImpl.kt
+++ src/main/kotlin/com/survival/kotlinstudy/datasource/UserDataSourceImpl.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 class UserDataSourceImpl : UserDataSource {
     override suspend fun getUsers(): List<User> {
-        val file = File("user.json")
+        val file = File("users.json")
 
         val data = Json.decodeFromString<List<User>>(file.readText())
         return data
