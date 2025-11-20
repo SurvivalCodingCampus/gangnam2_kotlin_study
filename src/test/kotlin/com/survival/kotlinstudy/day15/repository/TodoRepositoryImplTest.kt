45796d2130cdc88f13898cb27f25e4ba66a70d74
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-20 17:24:00 +0900

    feat: 연습문제 3번 테스트

--- src/test/kotlin/com/survival/kotlinstudy/day15/repository/TodoRepositoryImplTest.kt
+++ src/test/kotlin/com/survival/kotlinstudy/day15/repository/TodoRepositoryImplTest.kt
@@ -1,20 +1,52 @@
 package com.survival.kotlinstudy.day15.repository
 
-import com.survival.kotlinstudy.day15.datasource.FileTodoDataSourceImpl
+import com.survival.kotlinstudy.day15.datasource.MockTodoDataSourceImpl
 import com.survival.kotlinstudy.day15.model.Todo
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
-import kotlin.test.assertTrue
 
 class TodoRepositoryImplTest {
 
     @Test
-    fun `TodoRepositoryImpl 의 getTodos() 테스트`(): Unit = runBlocking {
-        val filePath = "data/todos.json"
-        val repository = TodoRepositoryImpl(FileTodoDataSourceImpl(filePath))
-        val expected = 200
+    fun `MockTodoRepository - getTodos() 테스트`(): Unit = runBlocking {
+        val text = """
+            [
+              {
+                "userId": 1,
+                "id": 1,
+                "title": "delectus aut autem",
+                "completed": false
+              },
+              {
+                "userId": 1,
+                "id": 2,
+                "title": "quis ut nam facilis et officia qui",
+                "completed": false
+              },
+              {
+                "userId": 1,
+                "id": 3,
+                "title": "fugiat veniam minus",
+                "completed": false
+              },
+              {
+                "userId": 1,
+                "id": 4,
+                "title": "et porro tempora",
+                "completed": true
+              },
+              {
+                "userId": 1,
+                "id": 5,
+                "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                "completed": false
+              }
+            ]
+        """.trimIndent()
+        val repository = TodoRepositoryImpl(MockTodoDataSourceImpl(text))
+        val expected = 5
 
 
         val list = repository.getTodos()
@@ -23,19 +55,49 @@ class TodoRepositoryImplTest {
         assertEquals(expected, list.size)
         assertIs<List<Todo>>(list)
 
-
     }
 
     @Test
     fun `TodoRepositoryImpl 의 getCompletedTodos() 테스트`(): Unit = runBlocking {
-        val filePath = "data/todos.json"
-        val repository = TodoRepositoryImpl(FileTodoDataSourceImpl(filePath))
-
+        val text = """
+            [
+              {
+                "userId": 1,
+                "id": 1,
+                "title": "delectus aut autem",
+                "completed": true
+              },
+              {
+                "userId": 1,
+                "id": 2,
+                "title": "quis ut nam facilis et officia qui",
+                "completed": true
+              },
+              {
+                "userId": 1,
+                "id": 3,
+                "title": "fugiat veniam minus",
+                "completed": false
+              },
+              {
+                "userId": 1,
+                "id": 4,
+                "title": "et porro tempora",
+                "completed": true
+              },
+              {
+                "userId": 1,
+                "id": 5,
+                "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                "completed": false
+              }
+            ]
+        """.trimIndent()
+        val repository = TodoRepositoryImpl(MockTodoDataSourceImpl(text))
+        val expected = 3
 
         val list = repository.getCompletedTodos()
 
-
-        assertTrue { list.any { it.completed } }
-
+        assertEquals(expected, list.size)
     }
 }
