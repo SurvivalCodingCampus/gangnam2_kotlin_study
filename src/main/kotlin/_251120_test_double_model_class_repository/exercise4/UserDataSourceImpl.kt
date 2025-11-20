ccb1b2c36ce3e3434986aeeda662e79355311c63
KunHee Jung <misterjerry@sju.ac.kr>
2025-11-21 00:37:31 +0900

    fix: FilePath에 의존적인 것을 생성자로 옮겨서 해결

--- src/main/kotlin/_251120_test_double_model_class_repository/exercise4/UserDataSourceImpl.kt
+++ src/main/kotlin/_251120_test_double_model_class_repository/exercise4/UserDataSourceImpl.kt
@@ -4,9 +4,9 @@ import _251120_test_double_model_class_repository.common_config.FILEPATH4
 import kotlinx.serialization.json.Json
 import java.io.File
 
-class UserDataSourceImpl : UserDataSource {
+class UserDataSourceImpl(val filePath: String) : UserDataSource {
     override suspend fun getAllUser(): List<User> {
-        val file = File(FILEPATH4).readText()
+        val file = File(filePath).readText()
         return Json.decodeFromString(file)
     }
 }
