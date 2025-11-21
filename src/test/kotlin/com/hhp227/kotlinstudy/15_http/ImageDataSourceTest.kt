b49aa0a68a630b50c11928de585b92951922f2e3
hhp227 <hong227@naver.com>
2025-11-22 01:51:56 +0900

    fix: ImageDataSource 테스트 코드 수정

--- src/test/kotlin/com/hhp227/kotlinstudy/15_http/ImageDataSourceTest.kt
+++ src/test/kotlin/com/hhp227/kotlinstudy/15_http/ImageDataSourceTest.kt
@@ -1,48 +1,88 @@
 package com.hhp227.kotlinstudy.`15_http`
 
+import com.hhp227.kotlinstudy.`15_http`.image.ImageDataSource
 import com.hhp227.kotlinstudy.`15_http`.image.ImageDataSourceImpl
+import io.ktor.client.*
+import io.ktor.client.engine.mock.*
+import io.ktor.http.*
+import io.ktor.utils.io.*
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
+import kotlinx.io.IOException
 import org.junit.Test
 import java.io.File
-import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
 import kotlin.test.assertIs
 import kotlin.test.assertTrue
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 class ImageDataSourceTest {
-    val imageDataSource = ImageDataSourceImpl()
+    lateinit var imageDataSource: ImageDataSource
+
+    fun createMockClient(
+        status: HttpStatusCode,
+        responseBody: ByteArray
+    ): HttpClient {
+        val mockEngine = MockEngine { request ->
+            respond(
+                content = ByteReadChannel(responseBody),
+                status = status,
+                headers = headersOf(HttpHeaders.ContentType, "image/jpeg")
+            )
+        }
+        return HttpClient(mockEngine)
+    }
 
     @Test
     fun `fetchImage 메소드 테스트`() = runTest {
+        val mockBytes = byteArrayOf(1, 2, 3, 4)
+        val client = createMockClient(HttpStatusCode.OK, mockBytes)
         val imageUrl = "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2023/07/28/3799c277-b41b-4a79-8d57-6f34f49ee29c.jpg"
+        imageDataSource = ImageDataSourceImpl(client)
         val byteArray = imageDataSource.fetchImage(imageUrl)
 
         assertTrue(byteArray.isNotEmpty())
         assertIs<ByteArray>(byteArray)
     }
 
+    @Test
+    fun `fetchImage 실패시 예외 발생 테스트`() = runTest {
+        val client = createMockClient(HttpStatusCode.NotFound, ByteArray(0))
+        imageDataSource = ImageDataSourceImpl(client)
+
+        assertFailsWith<IOException> {
+            imageDataSource.fetchImage("http://fakeurl.com/404.jpg")
+        }
+    }
+
     @OptIn(ExperimentalUuidApi::class)
     @Test
-    fun `saveImage 올바른 이미지가 아니면 에러가 나는지 테스트`() = runTest {
-        val imageUrl = "https://chatgpt.com/"
-        val byteArray = imageDataSource.fetchImage(imageUrl)
-        val filepath = "image1_${Uuid.random().toHexString()}.jpg"
+    fun `saveImage empty bytes 로 예외 발생 테스트`() = runTest {
+        val client = createMockClient(HttpStatusCode.OK, ByteArray(0))
+        val filePath = "image1_${Uuid.random().toHexString()}.jpg"
+        imageDataSource = ImageDataSourceImpl(client)
 
-        assertFails { imageDataSource.saveImage(byteArray, filepath) }
+        assertFailsWith<IllegalArgumentException> {
+            imageDataSource.saveImage(ByteArray(0), filePath)
+        }
     }
 
     @OptIn(ExperimentalUuidApi::class)
     @Test
     fun `saveImage 메소드로 이미지 저장하는지 테스트`() = runTest {
         val imageUrl = "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2023/07/28/3799c277-b41b-4a79-8d57-6f34f49ee29c.jpg"
-        val byteArray = imageDataSource.fetchImage(imageUrl)
-        val filepath = "image1_${Uuid.random().toHexString()}.jpg"
-        val file = File(filepath)
+        val mockBytes = byteArrayOf(1, 2, 3)
+        val client = createMockClient(HttpStatusCode.OK, mockBytes)
+        imageDataSource = ImageDataSourceImpl(client)
 
-        imageDataSource.saveImage(byteArray, filepath)
+        val filePath = "image1_${Uuid.random().toHexString()}.jpg"
+        val file = File(filePath)
+
+        imageDataSource.saveImage(mockBytes, filePath)
 
         assertTrue(file.exists())
+        assertEquals(mockBytes.size.toLong(), file.length())
         file.delete()
     }
 }
