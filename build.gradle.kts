ba6cba65c66e43c840e60703d4806bf92d06b43d
japygo <hyunnjinn@gmail.com>
2025-11-21 18:00:22 +0900

    feat: 연습문제 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 
     implementation("io.ktor:ktor-client-core:3.3.2")
     implementation("io.ktor:ktor-client-cio:3.3.2")
+    testImplementation("io.ktor:ktor-client-mock:3.3.2")
 }
 
 kotlin {
