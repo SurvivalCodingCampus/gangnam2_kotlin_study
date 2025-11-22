ff9b984fbcc2f379582952241e4bbecfd4abafb5
Neouul <fluctu0q0@gmail.com>
2025-11-22 15:31:54 +0900

    Chore: ktor-client 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")    // 직렬화 라이브러리
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")      // 코루틴
+
+    implementation("io.ktor:ktor-client-core:3.3.2")    // http 통신을 위한 공식 라이브러리 ktor-client
+    implementation("io.ktor:ktor-client-cio:3.3.2")
 }
 
 tasks.test {
