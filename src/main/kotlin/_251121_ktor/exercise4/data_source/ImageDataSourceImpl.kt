be6e2f893addfefd6b7980fd7537dedd84ba0625
MisterJerry <misterjerry12345@gmail.com>
2025-11-24 20:07:10 +0900

    feat: 연습문제4 ImageDataSource 설계 및 구현체 구현, 테스트코드 작성

---
+++ src/main/kotlin/_251121_ktor/exercise4/data_source/ImageDataSourceImpl.kt
@@ -0,0 +1,21 @@
+﻿package _251121_ktor.exercise4.data_source
+
+import _251121_ktor.exercise4.core.HttpClientFactory
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import java.io.File
+
+class ImageDataSourceImpl(
+    private val client: HttpClient = HttpClientFactory.create()
+) : ImageDataSource {
+    override suspend fun fetchImage(url: String): ByteArray {
+        val result = client.get(url)
+        return result.bodyAsBytes()
+    }
+
+    override suspend fun saveImage(bytes: ByteArray, path: String) {
+        File(path).writeBytes(bytes)
+    }
+}
+
