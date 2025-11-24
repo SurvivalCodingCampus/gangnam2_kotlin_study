c815301b32e6eeef73f63ff4af3dd479c8610d08
MisterJerry123 <misterjerry12345@gmail.com>
2025-11-24 22:33:20 +0900

    feat: 연습문제4 ImageRepository 설계 및 구현체 구현, 테스트코드 작성

--- src/main/kotlin/_251121_ktor/exercise4/repository/ImageRepositoryImpl.kt
+++ src/main/kotlin/_251121_ktor/exercise4/repository/ImageRepositoryImpl.kt
@@ -1,6 +1,8 @@
 ﻿package _251121_ktor.exercise4.repository
 
 import _251121_ktor.exercise4.data_source.ImageDataSource
+import kotlinx.io.files.FileNotFoundException
+import java.io.File
 
 class ImageRepositoryImpl(
     private val imageDataSource: ImageDataSource
@@ -11,12 +13,19 @@ class ImageRepositoryImpl(
 
     override suspend fun saveImages(urls: List<String>, directory: String) {
         urls.forEachIndexed { index, url ->
-            imageDataSource.saveImage(imageDataSource.fetchImage(url), directory + "$index")
+            imageDataSource.saveImage(imageDataSource.fetchImage(url), directory + "${index + 1}")
         }
     }
 
     override suspend fun saveImageIfNotExists(url: String, path: String): Boolean {
-        TODO("Not yet implemented")
+        val file = File(path)
+        try {
+            file.readText()
+            return true
+        } catch (e: FileNotFoundException) {
+            imageDataSource.saveImage(imageDataSource.fetchImage(url), path)
+            return false
+        }
     }
 }
 
