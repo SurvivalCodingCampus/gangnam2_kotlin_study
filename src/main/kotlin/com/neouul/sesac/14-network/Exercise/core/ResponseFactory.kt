7daba2c34e80afcb4d9ae0f43b9341387ee89f72
Neouul <fluctu0q0@gmail.com>
2025-11-24 18:52:21 +0900

    Feat: 네트워크 연습문제 1 - RemoteDataSource 메서드 반환형 수정

--- src/main/kotlin/com/neouul/sesac/14-network/Exercise/core/ResponseFactory.kt
+++ src/main/kotlin/com/neouul/sesac/14-network/Exercise/core/ResponseFactory.kt
@@ -3,12 +3,21 @@ package com.neouul.sesac.`14-network`.Exercise.core
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.util.toMap
+import kotlinx.serialization.json.Json
 
 object ResponseFactory {
-    suspend fun create(response: HttpResponse): Response<String> {
-        return Response<String>(response.status.value,
+    suspend inline fun <reified T>create(response: HttpResponse): Response<T> {
+        return Response<T>(response.status.value,
             response.headers.toMap(),
-            response.bodyAsText(),
+            Json.decodeFromString<T>(response.bodyAsText()),
+        )
+    }
+
+    // DELETE 결과로 Unit 반환을 위한 오버로딩
+    suspend fun create(response: HttpResponse): Response<Unit> {
+        return Response<Unit>(response.status.value,
+            response.headers.toMap(),
+            Unit,
         )
     }
 }
--- src/main/kotlin/com/neouul/sesac/14-network/Exercise/data_source/RemoteDataSourceImpl.kt
+++ src/main/kotlin/com/neouul/sesac/14-network/Exercise/data_source/RemoteDataSourceImpl.kt
@@ -14,56 +14,56 @@ class RemoteDataSourceImpl(
 ) : RemoteDataSource {
 
     // post 조회
-    override suspend fun getPosts(): Response<String> {
+    override suspend fun getPosts(): Response<List<Post>> {
         val response = client.get("https://jsonplaceholder.typicode.com/posts")
 
-        return ResponseFactory.create(response)
+        return ResponseFactory.create<List<Post>>(response)
     }
 
-    override suspend fun getPost(id: Int): Response<String> {
+    override suspend fun getPost(id: Int): Response<Post> {
         val response = client.get("https://jsonplaceholder.typicode.com/posts/$id")
 
-        return ResponseFactory.create(response)
+        return ResponseFactory.create<Post>(response)
     }
 
     // post 생성
-    override suspend fun createPost(post: Post): Response<String> {
+    override suspend fun createPost(post: Post): Response<Post> {
         val response = client.post("https://jsonplaceholder.typicode.com/posts") {
             contentType(ContentType.Application.Json)   // 서버에 전송할 컨텐츠 설정
             setBody(post)   // post 객체를 JSON으로 직렬화하여 본문에 포함
         }
 
-        return ResponseFactory.create(response)
+        return ResponseFactory.create<Post>(response)
     }
 
     // post 속성 전부 업데이트
     override suspend fun updatePost(
         id: Int,
         post: Post
-    ): Response<String> {
+    ): Response<Post> {
         val response = client.put("https://jsonplaceholder.typicode.com/posts/$id") {
             contentType(ContentType.Application.Json)
             setBody(post)
         }
 
-        return ResponseFactory.create(response)
+        return ResponseFactory.create<Post>(response)
     }
 
     // post의 일부 속성 업데이트
     override suspend fun patchPost(
         id: Int,
         post: Post
-    ): Response<String> {
+    ): Response<Post> {
         val response = client.patch("https://jsonplaceholder.typicode.com/posts/$id") {
             contentType(ContentType.Application.Json)
             setBody(post)
         }
 
-        return ResponseFactory.create(response)
+        return ResponseFactory.create<Post>(response)
     }
 
     // post 삭제
-    override suspend fun deletePost(id: Int): Response<String> {
+    override suspend fun deletePost(id: Int): Response<Unit> {
         // 삭제 요청은 본문 없이 URL 경로를 통해 ID를 전달
         val response = client.delete("https://jsonplaceholder.typicode.com/posts/$id")
 
