12eeb1ef734e8d80236243d269fb8577e34ef264
Neouul <fluctu0q0@gmail.com>
2025-11-24 18:53:38 +0900

    Chore: ktor-client-mock 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("junit:junit:4.13.1")
     testImplementation("io.mockk:mockk:1.13.12")    // 모킹(Mock)을 위한 mockk 라이브러리
+    testImplementation("io.ktor:ktor-client-mock:3.3.2")    // HTTP Mock 객체를 위한 ktor 라이브러리
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")    // 직렬화 라이브러리
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")      // 코루틴
