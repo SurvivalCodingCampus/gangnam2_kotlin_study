d052ba882f9400ec5105cc18deb3e371ca998a80
Jieun Lee <geniee1750@gmail.com>
2025-11-26 01:08:44 +0900

    [과제] chore: build.gradle 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     testImplementation("junit:junit:4.13.1")
     testImplementation("io.mockk:mockk:1.13.11")
     testImplementation("io.ktor:ktor-client-mock:3.3.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
