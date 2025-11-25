afdb9dfeb225afda3d7c82701ba611f6cc4ecb25
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-25 18:06:18 +0900

    feat: 연습문제 1번

--- src/main/kotlin/com/survival/kotlinstudy/day17/datasource/StoreDataSource.kt
+++ src/main/kotlin/com/survival/kotlinstudy/day17/datasource/StoreDataSource.kt
@@ -2,6 +2,6 @@ package com.survival.kotlinstudy.day17.datasource
 
 import com.survival.kotlinstudy.day17.dto.StoreListDto
 
-interface StoreDataSource{
+interface StoreDataSource {
     suspend fun getStores(): StoreListDto
 }
