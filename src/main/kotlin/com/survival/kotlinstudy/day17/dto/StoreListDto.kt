7235479e31f67dc52626a86a16c640f2044f7552
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2025-11-25 18:43:47 +0900

    feat: 연습문제 1번

--- src/main/kotlin/com/survival/kotlinstudy/day17/dto/StoreListDto.kt
+++ src/main/kotlin/com/survival/kotlinstudy/day17/dto/StoreListDto.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class StoreListDto(
-    val count: Int?,
-    val stores: List<StoreDto>?
+    val count: Int? = null,
+    val stores: List<StoreDto>? = null
 )
