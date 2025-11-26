940623a714019734777755fd986aba78ba76c845
이영현 <hong.git@yahoo.com>
2025-11-27 01:51:16 +0900

    feat: 프로덕션 코드에서 디버그 코드를 제거

--- src/main/kotlin/com/ezlevup/my/day251126/exercise/data_source/UserDataSourceImpl.kt
+++ src/main/kotlin/com/ezlevup/my/day251126/exercise/data_source/UserDataSourceImpl.kt
@@ -27,12 +27,9 @@ class UserDataSourceImpl(
     override suspend fun createUser(user: UserDto): Response<UserDto> = withContext(Dispatchers.IO) {
         val httpResponse = client.post("https://jsonplaceholder.typicode.com/users") {
             contentType(ContentType.Application.Json)
-            // 객체를 JSON으로 직렬화하여 본문에 포함
-            //setBody(user)
             setBody(Json.encodeToString(user))  // 수동 직렬화
         }
 
-        println(httpResponse.status)
         return@withContext httpResponse.toResponse<UserDto>()
     }
 }
