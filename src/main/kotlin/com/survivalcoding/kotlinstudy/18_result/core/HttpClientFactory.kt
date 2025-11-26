129ef4450c07e05d8b4a080e06e2276a8657e672
Jieun Lee <geniee1750@gmail.com>
2025-11-26 19:58:29 +0900

    [과제] feat: 연습문제 3 풀이 제출

--- src/main/kotlin/com/survivalcoding/kotlinstudy/18_result/core/HttpClientFactory.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/18_result/core/HttpClientFactory.kt
@@ -2,6 +2,7 @@ package com.survivalcoding.kotlinstudy.`18_result`.core
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 
@@ -15,6 +16,9 @@ object HttpClientFactory {
                     ignoreUnknownKeys = true    // 추가 필드 무시하고 파싱하기 위함
                     isLenient = true    // Json 문법 느슨하게 허용
                 }
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 10_000 // Http 요청 시간 초과 처리 자동
             }
         }
     }
