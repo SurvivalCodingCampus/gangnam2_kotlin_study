67057ac66e8a84ad7d8cb6019b037f3d31ab2ba7
이영현 <hong.git@yahoo.com>
2025-11-27 01:58:05 +0900

    feat: 중복된 에러 처리 로직을 공통 함수로 추출

--- src/main/kotlin/com/ezlevup/my/day251126/exercise/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/com/ezlevup/my/day251126/exercise/repository/UserRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.ezlevup.my.day251126.exercise.repository
 
 import com.ezlevup.my.core.RepositoryResult
+import com.ezlevup.my.core.Response
 import com.ezlevup.my.day251126.exercise.data_source.UserDataSource
 import com.ezlevup.my.day251126.exercise.data_source.UserDataSourceImpl
 import com.ezlevup.my.day251126.exercise.mapper.toUser
@@ -8,16 +9,17 @@ import com.ezlevup.my.day251126.exercise.mapper.toUserDto
 import com.ezlevup.my.day251126.exercise.model.NetworkError
 import com.ezlevup.my.day251126.exercise.model.User
 import io.ktor.client.plugins.*
-import io.ktor.network.sockets.*
-import kotlinx.io.IOException
 
 class UserRepositoryImpl(
     val userDataSource: UserDataSource = UserDataSourceImpl()
 ) : UserRepository {
 
-    override suspend fun getUser(id: Int): RepositoryResult<User, NetworkError> {
+    private suspend fun <T, R> executeWithErrorHandling(
+        block: suspend () -> Response<T>,
+        mapper: (T) -> R
+    ): RepositoryResult<R, NetworkError> {
         return try {
-            val response = userDataSource.getUser(id)
+            val response = block()
 
             val statusValue = response.status.value
             when (statusValue) {
@@ -27,16 +29,16 @@ class UserRepositoryImpl(
             }
 
             if (response.body != null) {
-                val user = response.body.toUser()
-                RepositoryResult.Success(user)
+                val result = mapper(response.body)
+                RepositoryResult.Success(result)
             } else {
                 RepositoryResult.Error(NetworkError.ParseError)
             }
         } catch (e: HttpRequestTimeoutException) {
             RepositoryResult.Error(NetworkError.Timeout)
-        } catch (e: SocketTimeoutException) {
+        } catch (e: java.net.SocketTimeoutException) {
             RepositoryResult.Error(NetworkError.Timeout)
-        } catch (e: IOException) {
+        } catch (e: java.io.IOException) {
             RepositoryResult.Error(NetworkError.NetworkUnavailable)
         } catch (e: Exception) {
             println(e.toString())
@@ -44,35 +46,25 @@ class UserRepositoryImpl(
         }
     }
 
+
+    override suspend fun getUser(id: Int): RepositoryResult<User, NetworkError> {
+        return executeWithErrorHandling(
+            block = { userDataSource.getUser(id) },
+            mapper = { it.toUser() }
+        )
+    }
+
     override suspend fun getUsers(): RepositoryResult<List<User>, NetworkError> {
-        return try {
-            val response = userDataSource.getUsers()
-            if (response.body != null) {
-                val users = response.body.map { it.toUser() }
-                RepositoryResult.Success(users)
-            } else {
-                RepositoryResult.Error(NetworkError.NetworkUnavailable)
-            }
-        } catch (e: IOException) {
-            RepositoryResult.Error(NetworkError.NetworkUnavailable)
-        } catch (e: SocketTimeoutException) {
-            RepositoryResult.Error(NetworkError.Timeout)
-        }
+        return executeWithErrorHandling(
+            block = { userDataSource.getUsers() },
+            mapper = { it.map { dto -> dto.toUser() } }
+        )
     }
 
     override suspend fun createUser(user: User): RepositoryResult<User, NetworkError> {
-        return try {
-            val response = userDataSource.createUser(user.toUserDto())
-            if (response.body != null) {
-                val userDto = response.body
-                RepositoryResult.Success(userDto.toUser())
-            } else {
-                RepositoryResult.Error(NetworkError.NetworkUnavailable)
-            }
-        } catch (e: IOException) {
-            RepositoryResult.Error(NetworkError.NetworkUnavailable)
-        } catch (e: SocketTimeoutException) {
-            RepositoryResult.Error(NetworkError.Timeout)
-        }
+        return executeWithErrorHandling(
+            block = { userDataSource.createUser(user.toUserDto()) },
+            mapper = { it.toUser() }
+        )
     }
 }
