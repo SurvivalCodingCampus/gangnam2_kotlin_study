000e93263398d9d82f5f7586becf80f44f7a0f5b
Luca <hr.jeon28@gmail.com>
2025-11-26 18:46:20 +0900

    feat: Photo 데이터 모델 및 RemoteDataSource 구현

--- src/main/kotlin/com/luca/kotlinstudy/_16_http/datasource/RemoteDataSourceImpl.kt
+++ src/main/kotlin/com/luca/kotlinstudy/_16_http/datasource/RemoteDataSourceImpl.kt
@@ -1,6 +1,6 @@
 package com.luca.kotlinstudy._16_http.datasource
 
-import com.luca.kotlinstudy._16_http.core.Response
+import com.luca.kotlinstudy.core.Response
 import com.luca.kotlinstudy._16_http.model.Post
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
