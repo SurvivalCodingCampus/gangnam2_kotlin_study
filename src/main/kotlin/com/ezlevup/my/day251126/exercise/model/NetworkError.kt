c8777555441cf12d08a132b4d9c889db60ca8dcb
이영현 <hong.git@yahoo.com>
2025-11-26 23:40:02 +0900

    feat: 연습문제 3

--- src/main/kotlin/com/ezlevup/my/day251126/exercise/model/NetworkError.kt
+++ src/main/kotlin/com/ezlevup/my/day251126/exercise/model/NetworkError.kt
@@ -4,6 +4,8 @@ sealed class NetworkError {
     object NetworkUnavailable : NetworkError()
     object Timeout : NetworkError()
     data class HttpError(val code: Int) : NetworkError()
+    data class HttpClientError(val code: Int) : NetworkError()
+    data class HttpServerError(val code: Int) : NetworkError()
     object ParseError : NetworkError()
     data class Unknown(val message: String) : NetworkError()
 }
--- src/main/kotlin/com/ezlevup/my/day251126/exercise/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/com/ezlevup/my/day251126/exercise/repository/UserRepositoryImpl.kt
@@ -7,25 +7,40 @@ import com.ezlevup.my.day251126.exercise.mapper.toUser
 import com.ezlevup.my.day251126.exercise.mapper.toUserDto
 import com.ezlevup.my.day251126.exercise.model.NetworkError
 import com.ezlevup.my.day251126.exercise.model.User
+import io.ktor.client.plugins.*
 import io.ktor.network.sockets.*
 import kotlinx.io.IOException
 
 class UserRepositoryImpl(
     val userDataSource: UserDataSource = UserDataSourceImpl()
 ) : UserRepository {
+
     override suspend fun getUser(id: Int): RepositoryResult<User, NetworkError> {
         return try {
             val response = userDataSource.getUser(id)
+
+            val statusValue = response.status.value
+            when (statusValue) {
+                in 400..499 -> return RepositoryResult.Error(NetworkError.HttpClientError(statusValue))
+                in 500..599 -> return RepositoryResult.Error(NetworkError.HttpServerError(statusValue))
+                !in 200..299 -> return RepositoryResult.Error(NetworkError.HttpError(statusValue))
+            }
+
             if (response.body != null) {
                 val user = response.body.toUser()
                 RepositoryResult.Success(user)
             } else {
-                RepositoryResult.Error(NetworkError.NetworkUnavailable)
+                RepositoryResult.Error(NetworkError.ParseError)
             }
-        } catch (e: IOException) {
-            RepositoryResult.Error(NetworkError.NetworkUnavailable)
+        } catch (e: HttpRequestTimeoutException) {
+            RepositoryResult.Error(NetworkError.Timeout)
         } catch (e: SocketTimeoutException) {
             RepositoryResult.Error(NetworkError.Timeout)
+        } catch (e: IOException) {
+            RepositoryResult.Error(NetworkError.NetworkUnavailable)
+        } catch (e: Exception) {
+            println(e.toString())
+            RepositoryResult.Error(NetworkError.NetworkUnavailable)
         }
     }
 
