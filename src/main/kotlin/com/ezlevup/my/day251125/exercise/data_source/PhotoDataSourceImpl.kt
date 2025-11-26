9164f153038e79c7fe37673dc907876bb819493b
이영현 <hong.git@yahoo.com>
2025-11-26 11:36:05 +0900

    feat: withContext(Dispatchers.IO) 추가.

--- src/main/kotlin/com/ezlevup/my/day251125/exercise/data_source/PhotoDataSourceImpl.kt
+++ src/main/kotlin/com/ezlevup/my/day251125/exercise/data_source/PhotoDataSourceImpl.kt
@@ -6,12 +6,14 @@ import com.ezlevup.my.core.toResponse
 import com.ezlevup.my.day251125.exercise.dto.PhotoDto
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class PhotoDataSourceImpl(
     private val client: HttpClient = HttpClientProvider.client,
 ) : PhotoDataSource {
-    override suspend fun getPhotos(): Response<List<PhotoDto>> {
+    override suspend fun getPhotos(): Response<List<PhotoDto>> = withContext(Dispatchers.IO) {
         val httpResponse = client.get("https://")
-        return httpResponse.toResponse<List<PhotoDto>>()
+        httpResponse.toResponse<List<PhotoDto>>()
     }
 }
