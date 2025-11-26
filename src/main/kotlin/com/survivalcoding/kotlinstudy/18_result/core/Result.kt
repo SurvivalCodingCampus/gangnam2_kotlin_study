4e93e792d924de6149a7550e3d876728fc77f7d7
Jieun Lee <geniee1750@gmail.com>
2025-11-26 18:35:50 +0900

    [과제] feat: 연습문제 1 풀이 제출

---
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/18_result/core/Result.kt
@@ -0,0 +1,11 @@
+package com.survivalcoding.kotlinstudy.`18_result`.core
+
+sealed class Result<out T, out E> {
+    data class Success<out T>(
+        val data: T
+    ) : Result<T, Nothing>()
+
+    data class Error<out E>(
+        val error: E
+    ) : Result<Nothing, E>()
+}
