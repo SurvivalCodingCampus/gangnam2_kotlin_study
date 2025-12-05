bfa6b0a0c227fd31dd425c208f0689e744c8559f
Neouul <fluctu0q0@gmail.com>
2025-12-05 16:38:29 +0900

    feat: Flow 연습문제 3

---
+++ src/main/kotlin/com/neouul/sesac/17-flow/Exercise3.kt
@@ -0,0 +1,29 @@
+package com.neouul.sesac.`17-flow`
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+@OptIn(FlowPreview::class)
+fun main() = runBlocking {
+    // 연습문제 3
+    val flow1 = withContext(Dispatchers.IO) {
+        flow{
+            emit("데이터 로딩 중")
+        }.flowOn(Dispatchers.Default)
+            .collect {
+                println(it)
+            }
+    }
+
+    val flow2 = flow{
+        while (true){
+            emit(readln())
+        }
+    }.debounce(3000)
+        .collect { println("Debounced: $it") }
+}
