396680a10a32e8f0154b149a7f1cff2b4910c544
Jieun Lee <geniee1750@gmail.com>
2025-12-08 22:28:59 +0900

    [과제] fix: 연습문제 2 테스트 위해 함수형으로 수정

--- src/main/kotlin/com/survivalcoding/kotlinstudy/20_flow/Exercise2.kt
+++ src/main/kotlin/com/survivalcoding/kotlinstudy/20_flow/Exercise2.kt
@@ -1,31 +1,38 @@
 package com.survivalcoding.kotlinstudy.`20_flow`
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.runBlocking
 
+// flow A : 300ms 간격으로 "A1", "A2", "A3"
+fun getFlowA(listA: List<String>) = flow {
+    for (a in listA) {
+        delay(300)
+        emit(a)
+    }
+}
+
+// flow B : 100ms 간격으로 B1", "B2", "B3", "B4"
+fun getFlowB(listB: List<String>) = flow {
+    for (b in listB) {
+        delay(100)
+        emit(b)
+    }
+}
+
+
+fun getZip(flowA: Flow<String>, flowB: Flow<String>) =
+    flowA.zip(flowB) { a, b -> "$a - $b" }
+
+fun getCombine(flowA: Flow<String>, flowB: Flow<String>) =
+    flowA.combine(flowB) { a, b -> "$a - $b" }
+
 fun main() = runBlocking {
-    val flowA = flow {
-        // flow A : 300ms 간격으로 "A1", "A2", "A3"
-        val listA = listOf("A1", "A2", "A3")
-
-        for (a in listA) {
-            delay(300)
-            emit(a)
-        }
-    }
-
-    val flowB = flow {
-        // flow B : 100ms 간격으로 B1", "B2", "B3", "B4"
-        val listB = listOf("B1", "B2", "B3", "B4")
-
-        for (b in listB) {
-            delay(100)
-            emit(b)
-        }
-    }
+    val flowA = getFlowA(listOf("A1", "A2", "A3"))
+    val flowB = getFlowB(listOf("B1", "B2", "B3", "B4"))
 
     /**
      * ZIP
@@ -37,7 +44,7 @@ fun main() = runBlocking {
      * B4는 매칭 상대가 없어서 버려짐
      */
     println("ZIP")
-    flowA.zip(flowB) { a, b -> "$a - $b" }
+    getZip(flowA, flowB)
         .collect { println(it) }
 
     /**
@@ -52,6 +59,6 @@ fun main() = runBlocking {
      * 새 값 발생 시마다 최신 조합을 사용
      */
     println("COMBINE")
-    flowA.combine(flowB) { a, b -> "$a - $b" }
+    getCombine(flowA, flowB)
         .collect { println(it) }
 }
